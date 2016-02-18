ebeeb3b2f919ac75772e7386c86cdb6130f75581
Raoul v. R <vanruesc@outlook.de>
2016-02-18 02:29:18 +0100

    Updated docs.

--- docs/api.js
+++ docs/api.js
@@ -4,7 +4,6 @@ YUI.add("yuidoc-meta", function(Y) {
         "AdaptiveLuminosityMaterial",
         "AdaptiveToneMappingPass",
         "BloomPass",
-        "BlurMaterial",
         "BokehMaterial",
         "BokehPass",
         "ClearMaskPass",
