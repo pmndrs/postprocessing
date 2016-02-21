87583f46c4ad6a695cfd749e1d2bd9410bb2c6a4
Raoul v. R <vanruesc@outlook.de>
2016-02-21 19:59:13 +0100

    Putting minification on hold for now.
    UglifyJS doesn't support ES2015 yet and can't deal with the keyword
    "let". Will include it again when it's been updated. Hopefully soon..

--- Gruntfile.js
+++ Gruntfile.js
@@ -108,7 +108,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-fs-inline");
 	grunt.loadNpmTasks("grunt-rollup");
 
-	grunt.registerTask("default", ["clean", "build", "uglify", "nodeunit"]);
+	//grunt.registerTask("default", ["clean", "build", "uglify", "nodeunit"]);
+	grunt.registerTask("default", ["clean", "build", "nodeunit"]);
 	grunt.registerTask("build", ["jshint", "fsinline", "rollup", "copy", "clean"]);
 	grunt.registerTask("test", ["jshint", "nodeunit"]);
 
