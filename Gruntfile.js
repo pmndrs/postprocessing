c1a3554101fbfd5a962e18fd7d006b83204b5545
Raoul v. R <vanruesc@outlook.de>
2016-07-20 12:11:10 +0200

    Switched to eslint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function(grunt) {
 	});
 
 	grunt.loadNpmTasks("grunt-contrib-nodeunit");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-eslint");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-yuidoc");
 	grunt.loadNpmTasks("grunt-contrib-clean");
