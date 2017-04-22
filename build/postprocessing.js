423ebb364efbaf51d965780d19713fb2453ad5e0
Raoul v. R <vanruesc@outlook.de>
2017-04-22 10:41:15 +0200

    Version 2.1.2.

--- build/postprocessing.js
+++ build/postprocessing.js
@@ -1,5 +1,5 @@
 /**
- * postprocessing v2.1.1 build Apr 17 2017
+ * postprocessing v2.1.2 build Apr 22 2017
  * https://github.com/vanruesc/postprocessing
  * Copyright 2017 Raoul van Rüschen, Zlib
  */
