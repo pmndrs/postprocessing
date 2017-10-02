e46edf509864481e35bf42ac9abc861381a2d705
Raoul v. R <vanruesc@outlook.de>
2017-10-02 19:43:50 +0200

    Renamed searchImage.js to searchImageDataUrl.js.
    Added a comment similar to the one in areaImageDataUrl.js.

---
+++ src/materials/images/smaa/searchImageDataUrl.js
@@ -0,0 +1,2 @@
+// Generated with SMAASearchImageData.generate().toCanvas().toDataUrl(), not cropped, low dynamic range.
+export default "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEIAAAAhCAAAAABIXyLAAAAAOElEQVRIx2NgGAWjYBSMglEwEICREYRgFBZBqDCSLA2MGPUIVQETE9iNUAqLR5gIeoQKRgwXjwAAGn4AtaFeYLEAAAAASUVORK5CYII";
