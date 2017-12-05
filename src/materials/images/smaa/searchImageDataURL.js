068da5b0beebc77c30d59f11458e18a202f4def2
Raoul v. R <vanruesc@users.noreply.github.com>
2017-12-05 01:26:03 +0100

    Updated and renamed searchImageDataUrl to searchImageDataURL.

--- src/materials/images/smaa/searchImageDataURL.js
+++ src/materials/images/smaa/searchImageDataURL.js
@@ -1,2 +1,2 @@
-// Generated with SMAASearchImageData.generate().toCanvas().toDataUrl(), not cropped, low dynamic range.
+// Generated with SMAASearchImageData.generate().toCanvas().toDataURL(), not cropped, low dynamic range.
 export default "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEIAAAAhCAAAAABIXyLAAAAAOElEQVRIx2NgGAWjYBSMglEwEICREYRgFBZBqDCSLA2MGPUIVQETE9iNUAqLR5gIeoQKRgwXjwAAGn4AtaFeYLEAAAAASUVORK5CYII";
