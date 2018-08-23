f86a6d791b52e86bdeaa2667f319e0ca1195caeb
Raoul v. R <vanruesc@users.noreply.github.com>
2018-08-24 00:14:32 +0200

    Allow setting the blend function via the constructor.

--- src/effects/DotScreenEffect.js
+++ src/effects/DotScreenEffect.js
@@ -1,4 +1,5 @@
 import { Uniform, Vector2 } from "three";
+import { BlendFunction } from "./blending/BlendFunction.js";
 import { Effect } from "./Effect.js";
 
 import fragment from "./glsl/dot-screen/shader.frag";
@@ -13,6 +14,7 @@ export class DotScreenEffect extends Effect {
 	 * Constructs a new dot screen effect.
 	 *
 	 * @param {Object} [options] - The options.
+	 * @param {BlendFunction} [options.blendFunction=BlendFunction.SCREEN] - The blend function of this effect.
 	 * @param {Number} [options.angle=1.57] - The angle of the dot pattern.
 	 * @param {Number} [options.scale=1.0] - The scale of the dot pattern.
 	 * @param {Number} [options.intensity=1.0] - The intensity of the effect.
@@ -21,12 +23,15 @@ export class DotScreenEffect extends Effect {
 	constructor(options = {}) {
 
 		const settings = Object.assign({
+			blendFunction: BlendFunction.NORMAL,
 			angle: Math.PI * 0.5,
 			scale: 1.0,
 			intensity: 1.0
 		}, options);
 
 		super("DotScreenEffect", fragment, {
+
+			blendFunction: settings.blendFunction,
 
 			uniforms: new Map([
 				["angle", new Uniform(new Vector2())],
