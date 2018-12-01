0aa95179b512e420e61bce061754ced56b3b1f9c
Raoul v. R <vanruesc@users.noreply.github.com>
2018-12-01 17:50:18 +0100

    Version 5.3.1.

--- build/postprocessing.js
+++ build/postprocessing.js
@@ -1,5 +1,5 @@
 /**
- * postprocessing v5.3.0 build Sat Dec 01 2018
+ * postprocessing v5.3.1 build Sat Dec 01 2018
  * https://github.com/vanruesc/postprocessing
  * Copyright 2018 Raoul van Rüschen, Zlib
  */
