3603a4a6505d7aa5cbabb57fc14f48f386fa198b
Raoul v. R <vanruesc@users.noreply.github.com>
2019-03-03 21:41:21 +0100

    Remove constructor parameters.
    Require a light position.

--- src/materials/GodRaysMaterial.js
+++ src/materials/GodRaysMaterial.js
@@ -1,4 +1,4 @@
-import { ShaderMaterial, Uniform, Vector2 } from "three";
+import { ShaderMaterial, Uniform } from "three";
 
 import fragment from "./glsl/god-rays/shader.frag";
 import vertex from "./glsl/common/shader.vert";
@@ -24,24 +24,10 @@ export class GodRaysMaterial extends ShaderMaterial {
 	/**
 	 * Constructs a new god rays material.
 	 *
-	 * @param {Vector2} [lightPosition] - The light position in screen space.
-	 * @param {Object} [options] - The options.
-	 * @param {Number} [options.density=0.96] - The density of the light rays.
-	 * @param {Number} [options.decay=0.93] - An illumination decay factor.
-	 * @param {Number} [options.weight=0.4] - A light ray weight factor.
-	 * @param {Number} [options.exposure=0.6] - A constant attenuation coefficient.
-	 * @param {Number} [options.clampMax=1.0] - An upper bound for the saturation of the overall effect.
+	 * @param {Vector2} lightPosition - The light position in screen space.
 	 */
 
-	constructor(lightPosition = new Vector2(), options = {}) {
-
-		const settings = Object.assign({
-			exposure: 0.6,
-			density: 0.93,
-			decay: 0.96,
-			weight: 0.4,
-			clampMax: 1.0
-		}, options);
+	constructor(lightPosition) {
 
 		super({
 
@@ -59,11 +45,11 @@ export class GodRaysMaterial extends ShaderMaterial {
 				inputBuffer: new Uniform(null),
 				lightPosition: new Uniform(lightPosition),
 
-				exposure: new Uniform(settings.exposure),
-				decay: new Uniform(settings.decay),
-				density: new Uniform(settings.density),
-				weight: new Uniform(settings.weight),
-				clampMax: new Uniform(settings.clampMax)
+				density: new Uniform(1.0),
+				decay: new Uniform(1.0),
+				weight: new Uniform(1.0),
+				exposure: new Uniform(1.0),
+				clampMax: new Uniform(1.0)
 
 			},
 
