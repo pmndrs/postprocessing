881c24fd53bbc25fe9858f905c819473a2e51676
Raoul v. R <vanruesc@users.noreply.github.com>
2019-03-03 22:28:00 +0100

    Update rollup-plugin-string.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from "rollup-plugin-babel";
 import minify from "rollup-plugin-babel-minify";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
-import string from "rollup-plugin-string";
+import { string } from "rollup-plugin-string";
 
 const pkg = require("./package.json");
 const date = (new Date()).toDateString();
