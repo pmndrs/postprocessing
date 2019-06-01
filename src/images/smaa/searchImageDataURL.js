6a28ab0135a654743b7f93bd6cf21712624c855f
Raoul v. R <vanruesc@users.noreply.github.com>
2019-06-01 18:12:46 +0200

    Switch to cropped, HDR search image

--- src/images/smaa/searchImageDataURL.js
+++ src/images/smaa/searchImageDataURL.js
@@ -1,2 +1,2 @@
-// Generated with SMAASearchImageData.generate().toCanvas().toDataURL(), not cropped, low dynamic range.
-export default "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEIAAAAhCAAAAABIXyLAAAAAOElEQVRIx2NgGAWjYBSMglEwEICREYRgFBZBqDCSLA2MGPUIVQETE9iNUAqLR5gIeoQKRgwXjwAAGn4AtaFeYLEAAAAASUVORK5CYII";
+// Generated with SMAASearchImageData.generate().toCanvas().toDataURL(), cropped, high dynamic range.
+export default "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAAAQCAYAAACm53kpAAAAeElEQVRYR+2XSwqAMAxEJ168ePEqwRSKhIIiuHjJqiU0gWE+1CQdApcVAMUAuARaMGCX1MIL/Ow13++9lW2s3mW9MWvsnWc/2fvGygwPAN4E8QzAA4CXAB6AHjG4JTHYI1ey3pcx6FHnEfhLDOIBKAmUBK6/ANUDTlROXAHd9EC1AAAAAElFTkSuQmCC";
