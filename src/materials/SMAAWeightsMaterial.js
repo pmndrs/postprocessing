12385d395232491d2281bb14aef2f64e91da4d0a
Raoul v. R <vanruesc@users.noreply.github.com>
2019-06-01 18:17:29 +0200

    Update SMAAWeightsMaterial

--- src/materials/SMAAWeightsMaterial.js
+++ src/materials/SMAAWeightsMaterial.js
@@ -15,9 +15,10 @@ export class SMAAWeightsMaterial extends ShaderMaterial {
 	 * Constructs a new SMAA weights material.
 	 *
 	 * @param {Vector2} [texelSize] - The absolute screen texel size.
+	 * @param {Vector2} [resolution] - The resolution.
 	 */
 
-	constructor(texelSize = new Vector2()) {
+	constructor(texelSize = new Vector2(), resolution = new Vector2()) {
 
 		super({
 
@@ -26,11 +27,16 @@ export class SMAAWeightsMaterial extends ShaderMaterial {
 			defines: {
 
 				// Configurable settings:
-				MAX_SEARCH_STEPS_INT: "8",
-				MAX_SEARCH_STEPS_FLOAT: "8.0",
+				MAX_SEARCH_STEPS_INT: "16",
+				MAX_SEARCH_STEPS_FLOAT: "16.0",
+				MAX_SEARCH_STEPS_DIAG_INT: "8",
+				MAX_SEARCH_STEPS_DIAG_FLOAT: "8.0",
+				CORNER_ROUNDING: "25",
+				CORNER_ROUNDING_NORM: "0.25",
 
 				// Non-configurable settings:
 				AREATEX_MAX_DISTANCE: "16.0",
+				AREATEX_MAX_DISTANCE_DIAG: "20.0",
 				AREATEX_PIXEL_SIZE: "(1.0 / vec2(160.0, 560.0))",
 				AREATEX_SUBTEX_SIZE: "(1.0 / 7.0)",
 				SEARCHTEX_SIZE: "vec2(66.0, 33.0)",
@@ -43,7 +49,8 @@ export class SMAAWeightsMaterial extends ShaderMaterial {
 				inputBuffer: new Uniform(null),
 				areaTexture: new Uniform(null),
 				searchTexture: new Uniform(null),
-				texelSize: new Uniform(texelSize)
+				texelSize: new Uniform(texelSize),
+				resolution: new Uniform(resolution)
 
 			},
 
@@ -70,8 +77,100 @@ export class SMAAWeightsMaterial extends ShaderMaterial {
 
 	setOrthogonalSearchSteps(steps) {
 
+		steps = Math.min(Math.max(steps, 0), 112);
+
 		this.defines.MAX_SEARCH_STEPS_INT = steps.toFixed("0");
 		this.defines.MAX_SEARCH_STEPS_FLOAT = steps.toFixed("1");
+		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Specifies the maximum steps performed in the diagonal pattern searches, at
+	 * each side of the pixel. This search jumps one pixel at time.
+	 *
+	 * On high-end machines this search is cheap (between 0.8x and 0.9x slower for
+	 * 16 steps), but it can have a significant impact on older machines.
+	 *
+	 * @param {Number} steps - The search steps. Range: [0, 20].
+	 */
+
+	setDiagonalSearchSteps(steps) {
+
+		steps = Math.min(Math.max(steps, 0), 20);
+
+		this.defines.MAX_SEARCH_STEPS_DIAG_INT = steps.toFixed("0");
+		this.defines.MAX_SEARCH_STEPS_DIAG_FLOAT = steps.toFixed("1");
+		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Specifies how much sharp corners will be rounded.
+	 *
+	 * @param {Number} rounding - The corner rounding amount. Range: [0, 100].
+	 */
+
+	setCornerRounding(rounding) {
+
+		rounding = Math.min(Math.max(rounding, 0), 100);
+
+		this.defines.CORNER_ROUNDING = rounding.toFixed("4");
+		this.defines.CORNER_ROUNDING_NORM = (rounding / 100.0).toFixed("4");
+		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Indicates whether diagonal pattern detection is enabled.
+	 *
+	 * @type {Boolean}
+	 */
+
+	get diagonalDetection() {
+
+		return (this.defines.DISABLE_DIAG_DETECTION === undefined);
+
+	}
+
+	/**
+	 * Enables or disables diagonal pattern detection.
+	 *
+	 * @type {Boolean}
+	 */
+
+	set diagonalDetection(value) {
+
+		value ? (delete this.defines.DISABLE_DIAG_DETECTION) :
+			(this.defines.DISABLE_DIAG_DETECTION = "1");
+
+		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Indicates whether corner rounding is enabled.
+	 *
+	 * @type {Boolean}
+	 */
+
+	get cornerRounding() {
+
+		return (this.defines.DISABLE_CORNER_DETECTION === undefined);
+
+	}
+
+	/**
+	 * Enables or disables corner rounding.
+	 *
+	 * @type {Boolean}
+	 */
+
+	set cornerRounding(value) {
+
+		value ? (delete this.defines.DISABLE_CORNER_DETECTION) :
+			(this.defines.DISABLE_CORNER_DETECTION = "1");
+
 		this.needsUpdate = true;
 
 	}
--- src/materials/glsl/smaa-weights/shader.vert
+++ src/materials/glsl/smaa-weights/shader.vert
@@ -1,4 +1,5 @@
 uniform vec2 texelSize;
+uniform vec2 resolution;
 
 varying vec2 vUv;
 varying vec4 vOffset[3];
@@ -7,14 +8,15 @@ varying vec2 vPixCoord;
 void main() {
 
 	vUv = position.xy * 0.5 + 0.5;
-	vPixCoord = vUv / texelSize;
+	vPixCoord = vUv * resolution;
 
 	// Offsets for the searches (see @PSEUDO_GATHER4).
-	vOffset[0] = vUv.xyxy + texelSize.xyxy * vec4(-0.25, 0.125, 1.25, 0.125); // Changed sign in Y and W components.
-	vOffset[1] = vUv.xyxy + texelSize.xyxy * vec4(-0.125, 0.25, -0.125, -1.25); //Changed sign in Y and W components.
+	vOffset[0] = vUv.xyxy + texelSize.xyxy * vec4(-0.25, -0.125, 1.25, -0.125);
+	vOffset[1] = vUv.xyxy + texelSize.xyxy * vec4(-0.125, -0.25, -0.125, 1.25);
 
 	// This indicates the ends of the loops.
-	vOffset[2] = vec4(vOffset[0].xz, vOffset[1].yw) + vec4(-2.0, 2.0, -2.0, 2.0) * texelSize.xxyy * MAX_SEARCH_STEPS_FLOAT;
+	vOffset[2] = vec4(vOffset[0].xz, vOffset[1].yw) +
+		vec4(-2.0, 2.0, -2.0, 2.0) * texelSize.xxyy * MAX_SEARCH_STEPS_FLOAT;
 
 	gl_Position = vec4(position.xy, 1.0, 1.0);
 
