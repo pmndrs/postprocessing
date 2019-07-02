29494101aa628bbe1e3daead7b433c4e0428f697
Raoul v. R <vanruesc@users.noreply.github.com>
2019-07-03 00:11:13 +0200

    Adjust build config

--- rollup.config.js
+++ rollup.config.js
@@ -15,43 +15,38 @@ const banner = `/**
 
 const production = (process.env.NODE_ENV === "production");
 const globals = { three: "THREE" };
-const external = ["three"];
 
 const lib = {
 
-	esm: {
-
+	module: {
 		input: "src/index.js",
-		external: external,
+		external: Object.keys(globals),
 		plugins: [resolve(), glsl({
 			include: ["**/*.frag", "**/*.vert"],
 			compress: production,
 			sourceMap: false
 		})],
 		output: [{
+			file: pkg.module,
+			format: "esm",
+			banner: banner,
+			globals: globals
+		}, {
 			file: pkg.main,
 			format: "esm",
+			banner: banner,
 			globals: globals
-		}].concat(!production ? [] : [
-			{
-				file: pkg.module,
-				format: "esm",
-				banner: banner,
-				globals: globals
-			}, {
-				file: pkg.main.replace(".js", ".min.js"),
-				format: "esm",
-				globals: globals
-			}
-		])
-
+		}, {
+			file: pkg.main.replace(".js", ".min.js"),
+			format: "esm",
+			globals: globals
+		}]
 	},
 
-	umd: {
-
+	main: {
 		input: pkg.main,
-		external: external,
-		plugins: production ? [babel()] : [],
+		external: Object.keys(globals),
+		plugins: [babel()],
 		output: {
 			file: pkg.main,
 			format: "umd",
@@ -59,17 +54,31 @@ const lib = {
 			banner: banner,
 			globals: globals
 		}
+	},
 
+	min: {
+		input: pkg.main.replace(".js", ".min.js"),
+		external: Object.keys(globals),
+		plugins: [minify({
+			bannerNewLine: true,
+			comments: false
+		}), babel()],
+		output: {
+			file: pkg.main.replace(".js", ".min.js"),
+			format: "umd",
+			name: pkg.name.replace(/-/g, "").toUpperCase(),
+			banner: banner,
+			globals: globals
+		}
 	}
 
 };
 
 const demo = {
 
-	esm: {
-
+	module: {
 		input: "demo/src/index.js",
-		external: external,
+		external: Object.keys(globals),
 		plugins: [resolve(), commonjs(), glsl({
 			include: ["**/*.frag", "**/*.vert"],
 			compress: production,
@@ -79,56 +88,44 @@ const demo = {
 			file: "public/demo/index.js",
 			format: "esm",
 			globals: globals
-		}].concat(!production ? [] : [{
+		}].concat(production ? [{
 			file: "public/demo/index.min.js",
 			format: "esm",
 			globals: globals
-		}])
-
+		}] : [])
 	},
 
-	iife: {
-
-		input: "public/demo/index.js",
-		external: external,
-		plugins: production ? [babel()] : [],
-		output: {
+	main: {
+		input: production ? "public/demo/index.js" : "demo/src/index.js",
+		external: Object.keys(globals),
+		plugins: production ? [babel()] : [resolve(), commonjs(), glsl({
+			include: ["**/*.frag", "**/*.vert"],
+			compress: false,
+			sourceMap: false
+		})],
+		output: [{
 			file: "public/demo/index.js",
 			format: "iife",
 			globals: globals
-		}
+		}]
+	},
 
+	min: {
+		input: "public/demo/index.min.js",
+		external: Object.keys(globals),
+		plugins: [minify({
+			comments: false
+		}), babel()],
+		output: {
+			file: "public/demo/index.min.js",
+			format: "iife",
+			globals: globals
+		}
 	}
 
 };
 
-export default [lib.esm, lib.umd, demo.esm, demo.iife].concat(production ? [{
-
-		input: lib.esm.output[2].file,
-		external: external,
-		plugins: [babel(), minify({
-			bannerNewLine: true,
-			comments: false
-		})],
-		output: {
-			file: lib.esm.output[2].file,
-			format: "umd",
-			name: pkg.name.replace(/-/g, "").toUpperCase(),
-			banner: banner,
-			globals: globals
-		}
-
-	}, {
-
-		input: demo.esm.output[1].file,
-		external: external,
-		plugins: [babel(), minify({
-			comments: false
-		})],
-		output: {
-			file: demo.esm.output[1].file,
-			format: "iife",
-			globals: globals
-		}
-
-}] : []);
+export default (production ? [
+	lib.module, lib.main, lib.min,
+	demo.module, demo.main, demo.min
+] : [demo.main]);
