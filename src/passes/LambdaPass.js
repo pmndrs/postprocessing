7d265860e8a10ffce5c7f9d1ce1b4189bc84b9da
Raoul v. R <vanruesc@users.noreply.github.com>
2020-11-02 15:06:05 +0100

    Add doc comment

--- src/passes/LambdaPass.js
+++ src/passes/LambdaPass.js
@@ -29,6 +29,16 @@ export class LambdaPass extends Pass {
 
 	}
 
+	/**
+	 * Executes the function.
+	 *
+	 * @param {WebGLRenderer} renderer - The renderer.
+	 * @param {WebGLRenderTarget} inputBuffer - A frame buffer that contains the result of the previous pass.
+	 * @param {WebGLRenderTarget} outputBuffer - A frame buffer that serves as the output render target unless this pass renders to screen.
+	 * @param {Number} [deltaTime] - The time between the last frame and the current one in seconds.
+	 * @param {Boolean} [stencilTest] - Indicates whether a stencil mask is active.
+	 */
+
 	render(renderer, inputBuffer, outputBuffer, deltaTime, stencilTest) {
 
 		this.f();
