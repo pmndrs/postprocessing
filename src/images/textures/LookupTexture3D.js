d6f7775b06c9db972837c9e01c89e32f23abb0be
Raoul v. R <vanruesc@users.noreply.github.com>
2020-12-08 00:44:50 +0100

    Create LookupTexture3D.js

---
+++ src/images/textures/LookupTexture3D.js
@@ -0,0 +1,149 @@
+import {
+	Color,
+	ClampToEdgeWrapping,
+	DataTexture,
+	DataTexture3D,
+	FloatType,
+	LinearFilter,
+	RGBFormat
+} from "three";
+
+/**
+ * A color.
+ *
+ * @type {Color}
+ * @private
+ */
+
+const c = new Color();
+
+/**
+ * A 3D lookup texture (LUT).
+ */
+
+export class LookupTexture3D extends DataTexture3D {
+
+	/**
+	 * Constructs a new lookup texture.
+	 *
+	 * LUTs are usually stored as sRGB textures and may need to be converted into
+	 * linear color space via `convertSRGBToLinear()`.
+	 *
+	 * @param {TypedArray} data - The data.
+	 * @param {Number} width - The width.
+	 * @param {Number} height - The height.
+	 * @param {Number} depth - The depth.
+	 */
+
+	constructor(data, width, height, depth) {
+
+		super(data, width, height, depth);
+
+		this.format = RGBFormat;
+		this.magFilter = LinearFilter;
+		this.wrapS = ClampToEdgeWrapping;
+		this.wrapT = ClampToEdgeWrapping;
+		this.wrapR = ClampToEdgeWrapping;
+
+	}
+
+	/**
+	 * The current texture type.
+	 *
+	 * @type {Number}
+	 */
+
+	/*get type() {
+
+		return this.type;
+
+	}*/
+
+	/**
+	 * Sets the texture type.
+	 *
+	 * @type {Number}
+	 */
+
+	/*set type(value) {
+
+		this.type = value;
+		this.data = (value === FloatType) ?
+			this.highPrecisionData :
+			this.createLowPrecisionData();
+
+	}*/
+
+	/**
+	 * Creates a low precision version of the LUT data if it doesn't exist yet.
+	 *
+	 * @private
+	 * @return {Uint8Array} The low precision data.
+	 */
+
+	createLowPrecisionData() {
+
+		const data = this.image.data;
+		let lowPrecisionData = data;
+
+		if(data instanceof Float32Array) {
+
+			lowPrecisionData = new Uint8Array(data.length);
+
+			for(let i = 0, l = data.length; i < l; ++i) {
+
+				lowPrecisionData[i] = data[i++] * 255;
+
+			}
+
+			console.log("Created low precision data", lowPrecisionData);
+
+		}
+
+		return lowPrecisionData;
+
+	}
+
+	/**
+	 * Converts this lookup texture into linear color space.
+	 *
+	 * @return {LookupTexture3D} This texture.
+	 */
+
+	convertSRGBToLinear() {
+
+		const data = this.image.data;
+
+		for(let i = 0, l = data.length; i < l; i += 3) {
+
+			c.fromArray(data, i).convertSRGBToLinear().toArray(data, i);
+
+		}
+
+		return this;
+
+	}
+
+	/**
+	 * Creates a new 2D data texture from this 3D LUT.
+	 *
+	 * @return {DataTexture} The texture.
+	 */
+
+	toTexture2D() {
+
+		const width = this.width * this.depth;
+		const height = this.height;
+
+		const texture = new DataTexture(this.image.data, width, height);
+		texture.format = this.format;
+		texture.type = this.type;
+		texture.magFilter = this.magFilter;
+		texture.wrapS = this.wrapS;
+		texture.wrapT = this.wrapT;
+
+		return texture;
+
+	}
+
+}
