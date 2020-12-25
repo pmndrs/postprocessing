7e80559307f4cddaebf42a44f343edde0a949389
Raoul v. R <vanruesc@users.noreply.github.com>
2020-12-25 18:13:47 +0100

    Export LookupTexture3D

--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,4 @@
+export { LookupTexture3D } from "./textures/LookupTexture3D";
 export { NoiseTexture } from "./textures/NoiseTexture";
 export { SMAAAreaImageData } from "./smaa/SMAAAreaImageData";
 export { SMAASearchImageData } from "./smaa/SMAASearchImageData";
