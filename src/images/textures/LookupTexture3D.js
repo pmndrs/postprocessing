e1835483556d8faacc9b4edc291d4af11f9421d0
Raoul v. R <vanruesc@users.noreply.github.com>
2021-01-01 22:01:33 +0100

    Use clamped array

--- src/images/textures/LookupTexture3D.js
+++ src/images/textures/LookupTexture3D.js
@@ -65,7 +65,7 @@ export class LookupTexture3D extends DataTexture3D {
 		if(this.type === FloatType) {
 
 			const floatData = this.image.data;
-			const uint8Data = new Uint8Array(floatData.length);
+			const uint8Data = new Uint8ClampedArray(floatData.length);
 
 			for(let i = 0, l = floatData.length; i < l; ++i) {
 
