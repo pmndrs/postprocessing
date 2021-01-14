6de9c2f68347b4736eb3838e7d0f74c174c28843
Raoul v. R <vanruesc@users.noreply.github.com>
2021-01-14 12:41:12 +0100

    Throw error instead of writing to console

--- src/loaders/LUTCubeLoader.js
+++ src/loaders/LUTCubeLoader.js
@@ -116,7 +116,7 @@ export class LUTCubeLoader extends Loader {
 
 		if(domainMin.x > domainMax.x || domainMin.y > domainMax.y || domainMin.z > domainMax.z) {
 
-			console.error("Invalid input domain");
+			throw new Error("Invalid input domain");
 
 			domainMin.set(0.0, 0.0, 0.0);
 			domainMax.set(1.0, 1.0, 1.0);
