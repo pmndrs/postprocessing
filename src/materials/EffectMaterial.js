7ea7add71177ebc44c0849da4e84365884f61ca4
Raoul v. R <vanruesc@users.noreply.github.com>
2022-02-02 21:39:17 +0100

    Deprecate depthPacking
    Replaced by getDepthPacking() and setDepthPacking().

--- src/materials/EffectMaterial.js
+++ src/materials/EffectMaterial.js
@@ -88,24 +88,48 @@ export class EffectMaterial extends ShaderMaterial {
 	/**
 	 * The current depth packing.
 	 *
-	 * @type {Number}
+	 * @type {DepthPackingStrategies}
+	 * @deprecated Use getDepthPacking() instead.
 	 */
 
 	get depthPacking() {
 
-		return Number(this.defines.DEPTH_PACKING);
+		return this.getDepthPacking();
 
 	}
 
 	/**
 	 * Sets the depth packing.
 	 *
-	 * Use `BasicDepthPacking` or `RGBADepthPacking` if your depth texture contains packed depth.
-	 *
-	 * @type {Number}
+	 * @type {DepthPackingStrategies}
+	 * @deprecated Use setDepthPacking() instead.
 	 */
 
 	set depthPacking(value) {
+
+		this.setDepthPacking(value);
+
+	}
+
+	/**
+	 * Returns the current depth packing strategy.
+	 *
+	 * @return {DepthPackingStrategies} The depth packing strategy.
+	 */
+
+	getDepthPacking() {
+
+		return Number(this.defines.DEPTH_PACKING);
+
+	}
+
+	/**
+	 * Sets the depth packing strategy.
+	 *
+	 * @param {DepthPackingStrategies} value - The depth packing strategy.
+	 */
+
+	setDepthPacking(value) {
 
 		this.defines.DEPTH_PACKING = value.toFixed(0);
 		this.needsUpdate = true;
