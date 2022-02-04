c347117747f8caa298f3f4cb561e8f23a467117f
Raoul v. R <vanruesc@users.noreply.github.com>
2022-02-04 20:56:17 +0100

    Add getters and setters
    setNormalBuffer(), setDepthBuffer(), setNormalDepthBuffer(), setNoiseTexture(), getSamples(), setSamples(), getRings(), setRings(), getIntensity(), setIntensity(), getFade(), setFade(), getBias(), setBias(), getMinRadiusScale(), setMinRadiusScale(), getRadius(), setRadius(), isDistanceScalingEnabled(), setDistanceScalingEnabled(), setDistanceCutoff(), setProximityCutoff().

--- src/materials/SSAOMaterial.js
+++ src/materials/SSAOMaterial.js
@@ -1,9 +1,12 @@
 import { Matrix4, NoBlending, PerspectiveCamera, ShaderMaterial, Uniform, Vector2 } from "three";
+
 import fragmentShader from "./glsl/ssao/shader.frag";
 import vertexShader from "./glsl/ssao/shader.vert";
 
 /**
  * A Screen Space Ambient Occlusion (SSAO) shader material.
+ *
+ * @implements {Resizable}
  */
 
 export class SSAOMaterial extends ShaderMaterial {
@@ -28,6 +31,7 @@ export class SSAOMaterial extends ShaderMaterial {
 				DEPTH_PACKING: "0"
 			},
 			uniforms: {
+				depthBuffer: new Uniform(null),
 				normalBuffer: new Uniform(null),
 				normalDepthBuffer: new Uniform(null),
 				noiseTexture: new Uniform(null),
@@ -55,6 +59,48 @@ export class SSAOMaterial extends ShaderMaterial {
 		this.toneMapped = false;
 
 		this.adoptCameraSettings(camera);
+
+		/**
+		 * The resolution.
+		 *
+		 * @type {Vector2}
+		 * @private
+		 */
+
+		this.resolution = new Vector2();
+
+		/**
+		 * The relative sampling radius.
+		 *
+		 * @type {Number}
+		 * @private
+		 */
+
+		this.radius = 1.0;
+
+	}
+
+	/**
+	 * Sets the normal buffer.
+	 *
+	 * @param {Number} value - The buffer.
+	 */
+
+	setNormalBuffer(value) {
+
+		this.uniforms.normalBuffer.value = value;
+
+	}
+
+	/**
+	 * Sets the depth buffer.
+	 *
+	 * @param {Number} value - The buffer.
+	 */
+
+	setDepthBuffer(value) {
+
+		this.uniforms.depthBuffer.value = value;
 
 	}
 
@@ -106,6 +152,287 @@ export class SSAOMaterial extends ShaderMaterial {
 
 		this.defines.DEPTH_PACKING = value.toFixed(0);
 		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Sets the normal-depth buffer.
+	 *
+	 * @param {Number} value - The buffer.
+	 */
+
+	setNormalDepthBuffer(value) {
+
+		this.uniforms.normalDepthBuffer.value = value;
+
+		if(value !== null) {
+
+			this.defines.NORMAL_DEPTH = "1";
+
+		} else {
+
+			delete this.defines.NORMAL_DEPTH;
+
+		}
+
+		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Sets the noise texture.
+	 *
+	 * @param {Number} value - The texture.
+	 */
+
+	setNoiseTexture(value) {
+
+		this.uniforms.noiseTexture.value = value;
+
+	}
+
+	/**
+	 * Returns the amount of occlusion samples per pixel.
+	 *
+	 * @return {Number} The sample count.
+	 */
+
+	getSamples() {
+
+		return Number(this.defines.SAMPLES_INT);
+
+	}
+
+	/**
+	 * Sets the amount of occlusion samples per pixel.
+	 *
+	 * @param {Number} value - The sample count.
+	 */
+
+	setSamples(value) {
+
+		this.defines.SAMPLES_INT = value.toFixed(0);
+		this.defines.SAMPLES_FLOAT = value.toFixed(1);
+		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Returns the amount of spiral turns in the occlusion sampling pattern.
+	 *
+	 * @return {Number} The radius.
+	 */
+
+	getRings() {
+
+		return Number(this.defines.SPIRAL_TURNS);
+
+	}
+
+	/**
+	 * Sets the amount of spiral turns in the occlusion sampling pattern.
+	 *
+	 * @param {Number} value - The radius.
+	 */
+
+	setRings(value) {
+
+		this.defines.SPIRAL_TURNS = value.toFixed(1);
+		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Returns the intensity.
+	 *
+	 * @return {Number} The intensity.
+	 */
+
+	getIntensity() {
+
+		return this.uniforms.intensity.value;
+
+	}
+
+	/**
+	 * Sets the intensity.
+	 *
+	 * @param {Number} value - The intensity.
+	 */
+
+	setIntensity(value) {
+
+		this.uniforms.intensity.value = value;
+
+	}
+
+	/**
+	 * Returns the depth fade factor.
+	 *
+	 * @return {Number} The fade factor.
+	 */
+
+	getFade() {
+
+		return this.uniforms.fade.value;
+
+	}
+
+	/**
+	 * Sets the depth fade factor.
+	 *
+	 * @param {Number} value - The fade factor.
+	 */
+
+	setFade(value) {
+
+		this.uniforms.bias.value = value;
+
+	}
+
+	/**
+	 * Returns the depth bias.
+	 *
+	 * @return {Number} The bias.
+	 */
+
+	getBias() {
+
+		return this.uniforms.bias.value;
+
+	}
+
+	/**
+	 * Sets the depth bias.
+	 *
+	 * @param {Number} value - The bias.
+	 */
+
+	setBias(value) {
+
+		this.uniforms.bias.value = value;
+
+	}
+
+	/**
+	 * Returns the minimum radius scale for distance scaling.
+	 *
+	 * @return {Number} The minimum radius scale.
+	 */
+
+	getMinRadiusScale() {
+
+		return this.uniforms.minRadiusScale.value;
+
+	}
+
+	/**
+	 * Sets the minimum radius scale for distance scaling.
+	 *
+	 * @param {Number} value - The minimum radius scale.
+	 */
+
+	setMinRadiusScale(value) {
+
+		this.uniforms.minRadiusScale.value = value;
+
+	}
+
+	/**
+	 * Returns the occlusion sampling radius.
+	 *
+	 * @return {Number} The radius.
+	 */
+
+	getRadius() {
+
+		return Number(this.defines.RADIUS);
+
+	}
+
+	/**
+	 * Sets the occlusion sampling radius.
+	 *
+	 * @param {Number} value - The radius. Range [1e-6, 1.0].
+	 */
+
+	setRadius(value) {
+
+		this.radius = Math.min(Math.max(value, 1e-6), 1.0);
+		const radius = this.radius * this.resolution.height;
+		this.defines.RADIUS = radius.toFixed(11);
+		this.defines.RADIUS_SQ = (radius * radius).toFixed(11);
+		this.needsUpdate = true;
+
+	}
+
+	/**
+	 * Indicates whether distance-based radius scaling is enabled.
+	 *
+	 * @return {Boolean} Whether distance scaling is enabled.
+	 */
+
+	isDistanceScalingEnabled() {
+
+		return (this.defines.DISTANCE_SCALING !== undefined);
+
+	}
+
+	/**
+	 * Enables or disables distance-based radius scaling.
+	 *
+	 * @param {Boolean} value - Whether distance scaling should be enabled.
+	 */
+
+	setDistanceScalingEnabled(value) {
+
+		if(this.isDistanceScalingEnabled() !== value) {
+
+			if(value) {
+
+				this.defines.DISTANCE_SCALING = "1";
+
+			} else {
+
+				delete this.defines.DISTANCE_SCALING;
+
+			}
+
+			this.needsUpdate = true;
+
+		}
+
+	}
+
+	/**
+	 * Sets the occlusion distance cutoff.
+	 *
+	 * @param {Number} threshold - The distance threshold. Range [0.0, 1.0].
+	 * @param {Number} falloff - The falloff. Range [0.0, 1.0].
+	 */
+
+	setDistanceCutoff(threshold, falloff) {
+
+		this.uniforms.distanceCutoff.value.set(
+			Math.min(Math.max(threshold, 0.0), 1.0),
+			Math.min(Math.max(threshold + falloff, 0.0), 1.0)
+		);
+
+	}
+
+	/**
+	 * Sets the occlusion proximity cutoff.
+	 *
+	 * @param {Number} threshold - The range threshold. Range [0.0, 1.0].
+	 * @param {Number} falloff - The falloff. Range [0.0, 1.0].
+	 */
+
+	setProximityCutoff(threshold, falloff) {
+
+		this.uniforms.proximityCutoff.value.set(
+			Math.min(Math.max(threshold, 0.0), 1.0),
+			Math.min(Math.max(threshold + falloff, 0.0), 1.0)
+		);
 
 	}
 
