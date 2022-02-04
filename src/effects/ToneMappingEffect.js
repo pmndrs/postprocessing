323a92d95e6401a8bab302773592835e94f1b2fb
Raoul v. R <vanruesc@users.noreply.github.com>
2022-02-04 21:16:11 +0100

    Move ToneMappingMode enum up

--- src/effects/ToneMappingEffect.js
+++ src/effects/ToneMappingEffect.js
@@ -13,6 +13,25 @@ import { Effect } from "./Effect";
 import fragmentShader from "./glsl/tone-mapping/shader.frag";
 
 /**
+ * A tone mapping mode enumeration.
+ *
+ * @type {Object}
+ * @property {Number} REINHARD - Simple Reinhard tone mapping.
+ * @property {Number} REINHARD2 - Modified Reinhard tone mapping.
+ * @property {Number} REINHARD2_ADAPTIVE - Simulates the optic nerve responding to the amount of light it is receiving.
+ * @property {Number} OPTIMIZED_CINEON - Optimized filmic operator by Jim Hejl and Richard Burgess-Dawson.
+ * @property {Number} ACES_FILMIC - ACES tone mapping with a scale of 1.0/0.6.
+ */
+
+export const ToneMappingMode = {
+	REINHARD: 0,
+	REINHARD2: 1,
+	REINHARD2_ADAPTIVE: 2,
+	OPTIMIZED_CINEON: 3,
+	ACES_FILMIC: 4
+};
+
+/**
  * A tone mapping effect.
  *
  * Note: `ToneMappingMode.REINHARD2_ADAPTIVE` requires support for `EXT_shader_texture_lod` and
@@ -332,24 +351,3 @@ export class ToneMappingEffect extends Effect {
 	}
 
 }
-
-/**
- * A tone mapping mode enumeration.
- *
- * @type {Object}
- * @property {Number} REINHARD - Simple Reinhard tone mapping.
- * @property {Number} REINHARD2 - Modified Reinhard tone mapping.
- * @property {Number} REINHARD2_ADAPTIVE - Simulates the optic nerve responding to the amount of light it is receiving.
- * @property {Number} OPTIMIZED_CINEON - Optimized filmic operator by Jim Hejl and Richard Burgess-Dawson.
- * @property {Number} ACES_FILMIC - ACES tone mapping with a scale of 1.0/0.6.
- */
-
-export const ToneMappingMode = {
-
-	REINHARD: 0,
-	REINHARD2: 1,
-	REINHARD2_ADAPTIVE: 2,
-	OPTIMIZED_CINEON: 3,
-	ACES_FILMIC: 4
-
-};
