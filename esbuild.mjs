51928cdac1fe6c6d21bd15f0ee066896678923dd
Raoul v. R <vanruesc@users.noreply.github.com>
2022-02-21 11:56:48 +0100

    Adjust license date

--- esbuild.mjs
+++ esbuild.mjs
@@ -13,7 +13,7 @@ const plugins = [glsl({ minify })];
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date}
  * ${pkg.homepage}
- * Copyright ${date.slice(-4)} ${pkg.author.name}
+ * Copyright 2015-${date.slice(-4)} ${pkg.author.name}
  * @license ${pkg.license}
  */`;
 
