85423356d0d1130f61de6914938a21491177266a
Raoul v. R <vanruesc@users.noreply.github.com>
2022-03-17 13:38:01 +0100

    Manage scroll position

---
+++ manual/js/src/components/NavigationManager.js
@@ -0,0 +1,50 @@
+/**
+ * A navigation manager.
+ *
+ * @implements {Initializable}
+ */
+
+export class NavigationManager {
+
+	initialize() {
+
+		const nav = document.querySelector(".navigation");
+		const main = document.getElementById("main");
+		nav.scrollTop = Number(sessionStorage.getItem("nav-scroll"));
+		main.scrollTop = Number(sessionStorage.getItem("main-scroll"));
+		sessionStorage.removeItem("nav-scroll");
+		sessionStorage.removeItem("main-scroll");
+
+		let saveMainScroll = true;
+
+		function resetMainScroll() {
+
+			saveMainScroll = false;
+
+		}
+
+		for(const a of nav.querySelectorAll("a")) {
+
+			if(a.target !== "_blank") {
+
+				a.addEventListener("click", resetMainScroll);
+
+			}
+
+		}
+
+		window.addEventListener("beforeunload", () => {
+
+			sessionStorage.setItem("nav-scroll", nav.scrollTop.toFixed(0));
+
+			if(saveMainScroll) {
+
+				sessionStorage.setItem("main-scroll", main.scrollTop.toFixed(0));
+
+			}
+
+		});
+
+	}
+
+}
--- manual/js/src/components/index.js
+++ manual/js/src/components/index.js
@@ -1,3 +1,4 @@
+export { NavigationManager } from "./NavigationManager";
 export { SidebarManager } from "./SidebarManager";
 export { ThemeManager } from "./ThemeManager";
 export { ViewportManager } from "./ViewportManager";
