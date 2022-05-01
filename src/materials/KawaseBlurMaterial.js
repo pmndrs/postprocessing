0ce94aba708b76e1ef02585eaef8fc55ec286467
Raoul v. R <vanruesc@users.noreply.github.com>
2022-05-01 22:52:34 +0200

    Combine texelSize and halfTexelSize

--- src/materials/KawaseBlurMaterial.js
+++ src/materials/KawaseBlurMaterial.js
@@ -1,10 +1,10 @@
-import { NoBlending, ShaderMaterial, Uniform, Vector2 } from "three";
+import { NoBlending, ShaderMaterial, Uniform, Vector4 } from "three";
 
 import fragmentShader from "./glsl/convolution/kawase.frag";
 import vertexShader from "./glsl/convolution/kawase.vert";
 
 /**
- * An optimised convolution shader material.
+ * An optimized convolution shader material.
  *
  * Based on the GDC2003 Presentation by Masaki Kawase, Bunkasha Games:
  *  Frame Buffer Postprocessing Effects in DOUBLE-S.T.E.A.L (Wreckless)
@@ -23,17 +23,16 @@ export class KawaseBlurMaterial extends ShaderMaterial {
 	 * Constructs a new convolution material.
 	 *
 	 * TODO Remove texelSize param.
-	 * @param {Vector2} [texelSize] - Deprecated.
+	 * @param {Vector4} [texelSize] - Deprecated.
 	 */
 
-	constructor(texelSize = new Vector2()) {
+	constructor(texelSize = new Vector4()) {
 
 		super({
 			name: "KawaseBlurMaterial",
 			uniforms: {
 				inputBuffer: new Uniform(null),
-				texelSize: new Uniform(new Vector2()),
-				halfTexelSize: new Uniform(new Vector2()),
+				texelSize: new Uniform(new Vector4()),
 				kernel: new Uniform(0.0),
 				scale: new Uniform(1.0)
 			},
@@ -174,8 +173,7 @@ export class KawaseBlurMaterial extends ShaderMaterial {
 
 	setTexelSize(x, y) {
 
-		this.uniforms.texelSize.value.set(x, y);
-		this.uniforms.halfTexelSize.value.set(x, y).multiplyScalar(0.5);
+		this.uniforms.texelSize.value.set(x, y, x * 0.5, y * 0.5);
 
 	}
 
@@ -188,9 +186,8 @@ export class KawaseBlurMaterial extends ShaderMaterial {
 
 	setSize(width, height) {
 
-		const uniforms = this.uniforms;
-		uniforms.texelSize.value.set(1.0 / width, 1.0 / height);
-		uniforms.halfTexelSize.value.copy(uniforms.texelSize.value).multiplyScalar(0.5);
+		const x = 1.0 / width, y = 1.0 / height;
+		this.uniforms.texelSize.value.set(x, y, x * 0.5, y * 0.5);
 
 	}
 
