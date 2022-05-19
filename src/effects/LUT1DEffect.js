e3b589e4d4102df1fd4d6ca0b7d8934fd0616541
Don McCurdy <dm@donmccurdy.com>
2022-05-19 19:53:44 -0400

    Add LUT1DEffect
    - Renames LUTEffect → LUT3DEffect
    - Adds LUT1DEffect
    - Update DataTexture3D → Data3DTexture
    - Include 'inputEncoding' as a LUT3DEffect constructor parameter

---
+++ src/effects/LUT1DEffect.js
@@ -0,0 +1,52 @@
+import { FloatType, HalfFloatType, Uniform } from "three";
+import { BlendFunction } from "./blending/BlendFunction";
+import { Effect } from "./Effect";
+
+import fragmentShader from "./glsl/lut-1d/shader.frag";
+
+export class LUT1DEffect extends Effect {
+
+	/**
+	 * Constructs a new color grading effect.
+	 *
+	 * @param {Texture} lut - The lookup texture.
+	 * @param {Object} [options] - The options.
+	 * @param {BlendFunction} [options.blendFunction=BlendFunction.NORMAL] - The blend function of this effect.
+	 */
+
+	constructor(lut, { blendFunction = BlendFunction.NORMAL } = {}) {
+
+		super("LUT1DEffect", fragmentShader, {
+			blendFunction,
+			uniforms: new Map([["lut", new Uniform(null)]])
+		});
+
+		this.lut = lut;
+
+	}
+
+	/**
+	 * The LUT.
+	 *
+	 * @type {Texture}
+	 */
+
+	get lut() {
+
+		return this.uniforms.get("lut").value;
+
+	}
+
+	set lut(value) {
+
+		this.uniforms.get("lut").value = value;
+
+		if(value && (value.type === FloatType || value.type === HalfFloatType)) {
+
+			this.defines.set("LUT_PRECISION_HIGH", "1");
+
+		}
+
+	}
+
+}
