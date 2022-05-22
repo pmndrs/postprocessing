394757b0ebdd2fb1f6d9d6e1b4e8bf7206c8e0ef
Raoul v. R <vanruesc@users.noreply.github.com>
2022-05-22 15:12:33 +0200

    Adjust doc comments

--- src/core/KernelSize.js
+++ src/core/KernelSize.js
@@ -3,11 +3,11 @@
  *
  * @type {Object}
  * @property {Number} VERY_SMALL - A very small kernel that matches a 7x7 Gaussian blur kernel.
- * @property {Number} SMALL - A small kernel that matches a 15x15 Gaussian blur kernel.
- * @property {Number} MEDIUM - A medium sized kernel that matches a 23x23 Gaussian blur kernel.
- * @property {Number} LARGE - A large kernel that matches a 35x35 Gaussian blur kernel.
- * @property {Number} VERY_LARGE - A very large kernel that matches a 63x63 Gaussian blur kernel.
- * @property {Number} HUGE - A huge kernel that matches a 127x127 Gaussian blur kernel.
+ * @property {Number} SMALL - A small kernel that matches a 25x25 Gaussian blur kernel.
+ * @property {Number} MEDIUM - A medium sized kernel that matches a 63x63 Gaussian blur kernel.
+ * @property {Number} LARGE - A large kernel that matches a 127x127 Gaussian blur kernel.
+ * @property {Number} VERY_LARGE - A very large kernel that matches a 391x391 Gaussian blur kernel.
+ * @property {Number} HUGE - A huge kernel that cannot be replicated with a single Gaussian blur pass.
  */
 
 export const KernelSize = {
