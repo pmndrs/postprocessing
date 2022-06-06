b676bd0706653cd6ed94a8ba43a9a17fb2d2acfa
Raoul v. R <vanruesc@users.noreply.github.com>
2022-06-06 14:49:08 +0200

    Add FXAA demo

---
+++ manual/js/src/demos/fxaa.js
@@ -0,0 +1,147 @@
+import {
+	ColorManagement,
+	CubeTextureLoader,
+	LoadingManager,
+	PerspectiveCamera,
+	Scene,
+	sRGBEncoding,
+	VSMShadowMap,
+	WebGLRenderer
+} from "three";
+
+import {
+	BlendFunction,
+	EffectComposer,
+	EffectPass,
+	FXAAEffect,
+	RenderPass
+} from "postprocessing";
+
+import { Pane } from "tweakpane";
+import { ControlMode, SpatialControls } from "spatial-controls";
+import { calculateVerticalFoV, FPSMeter } from "../utils";
+import * as CornellBox from "../objects/CornellBox";
+
+function load() {
+
+	const assets = new Map();
+	const loadingManager = new LoadingManager();
+	const cubeTextureLoader = new CubeTextureLoader(loadingManager);
+
+	const path = document.baseURI + "img/textures/skies/sunset/";
+	const format = ".png";
+	const urls = [
+		path + "px" + format, path + "nx" + format,
+		path + "py" + format, path + "ny" + format,
+		path + "pz" + format, path + "nz" + format
+	];
+
+	return new Promise((resolve, reject) => {
+
+		loadingManager.onLoad = () => resolve(assets);
+		loadingManager.onError = (url) => reject(new Error(`Failed to load ${url}`));
+
+		cubeTextureLoader.load(urls, (t) => {
+
+			t.encoding = sRGBEncoding;
+			assets.set("sky", t);
+
+		});
+
+	});
+
+}
+
+window.addEventListener("load", () => load().then((assets) => {
+
+	ColorManagement.legacyMode = false;
+
+	// Renderer
+
+	const renderer = new WebGLRenderer({
+		powerPreference: "high-performance",
+		antialias: false,
+		stencil: false,
+		depth: false
+	});
+
+	renderer.debug.checkShaderErrors = (window.location.hostname === "localhost");
+	renderer.physicallyCorrectLights = true;
+	renderer.outputEncoding = sRGBEncoding;
+	renderer.shadowMap.type = VSMShadowMap;
+	renderer.shadowMap.autoUpdate = false;
+	renderer.shadowMap.needsUpdate = true;
+	renderer.shadowMap.enabled = true;
+
+	const container = document.querySelector(".viewport");
+	container.prepend(renderer.domElement);
+
+	// Camera & Controls
+
+	const camera = new PerspectiveCamera();
+	const controls = new SpatialControls(camera.position, camera.quaternion, renderer.domElement);
+	const settings = controls.settings;
+	settings.general.setMode(ControlMode.THIRD_PERSON);
+	settings.rotation.setSensitivity(2.2);
+	settings.rotation.setDamping(0.05);
+	settings.zoom.setDamping(0.1);
+	settings.translation.setEnabled(false);
+	controls.setPosition(0, 0, 5);
+
+	// Scene, Lights, Objects
+
+	const scene = new Scene();
+	scene.background = assets.get("sky");
+	scene.add(CornellBox.createLights());
+	scene.add(CornellBox.createEnvironment());
+	scene.add(CornellBox.createActors());
+
+	// Post Processing
+
+	const effect = new FXAAEffect();
+	const composer = new EffectComposer(renderer);
+	composer.addPass(new RenderPass(scene, camera));
+	composer.addPass(new EffectPass(camera, effect));
+
+	// Settings
+
+	const fpsMeter = new FPSMeter();
+	const pane = new Pane({ container: container.querySelector(".tp") });
+	pane.addMonitor(fpsMeter, "fps", { label: "FPS" });
+
+	const folder = pane.addFolder({ title: "Settings" });
+	folder.addInput(effect, "samples", { min: 0, max: 24, step: 1 });
+	folder.addInput(effect, "minEdgeThreshold", { min: 0.01, max: 0.3, step: 1e-4 });
+	folder.addInput(effect, "maxEdgeThreshold", { min: 0.01, max: 0.3, step: 1e-4 });
+	folder.addInput(effect, "subpixelQuality", { min: 0, max: 1.0, step: 1e-4 });
+
+	folder.addInput(effect.blendMode.opacity, "value", { label: "opacity", min: 0, max: 1, step: 0.01 });
+	folder.addInput(effect.blendMode, "blendFunction", { options: BlendFunction });
+
+	// Resize Handler
+
+	function onResize() {
+
+		const width = container.clientWidth, height = container.clientHeight;
+		camera.aspect = width / height;
+		camera.fov = calculateVerticalFoV(90, Math.max(camera.aspect, 16 / 9));
+		camera.updateProjectionMatrix();
+		composer.setSize(width, height);
+
+	}
+
+	window.addEventListener("resize", onResize);
+	onResize();
+
+	// Render Loop
+
+	requestAnimationFrame(function render(timestamp) {
+
+		fpsMeter.update(timestamp);
+		controls.update(timestamp);
+		composer.render();
+		requestAnimationFrame(render);
+
+	});
+
+}));
