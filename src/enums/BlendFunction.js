4a77f6d84bcaecf71c08a06aa6a414317176cc27
Raoul v. R <vanruesc@users.noreply.github.com>
2022-07-30 19:27:58 +0200

    Mark blend functions that support HDR

--- src/enums/BlendFunction.js
+++ src/enums/BlendFunction.js
@@ -10,35 +10,35 @@
  * @type {Object}
  * @property {Number} SKIP - Deprecated. Use DST instead. Warning: This blend function does NOT fully disable the effect.
  * @property {Number} SET - Deprecated. Use SRC instead.
- * @property {Number} ADD - Additive blending. Fast, but may produce washed out results.
- * @property {Number} ALPHA - Alpha blending. Blends based on the alpha value of the new color.
- * @property {Number} AVERAGE - Calculates the avarage of the new color and the base color.
+ * @property {Number} ADD - Additive blending. Supports HDR.
+ * @property {Number} ALPHA - Alpha blending. Blends based on the alpha value of the new color. Supports HDR.
+ * @property {Number} AVERAGE - Calculates the avarage of the new color and the base color. Supports HDR.
  * @property {Number} COLOR - Converts the colors to HSL and blends based on color.
  * @property {Number} COLOR_BURN - Color burn.
  * @property {Number} COLOR_DODGE - Color dodge.
- * @property {Number} DARKEN - Prioritize darker colors.
- * @property {Number} DIFFERENCE - Color difference.
- * @property {Number} DIVIDE - Color division.
- * @property {Number} DST - Overwrites the new color with the base color. Ignores opacity.
+ * @property {Number} DARKEN - Prioritize darker colors. Supports HDR.
+ * @property {Number} DIFFERENCE - Color difference. Supports HDR.
+ * @property {Number} DIVIDE - Color division. Supports HDR.
+ * @property {Number} DST - Overwrites the new color with the base color while ignoring opacity. Supports HDR.
  * @property {Number} EXCLUSION - Color exclusion.
  * @property {Number} HARD_LIGHT - Hard light.
  * @property {Number} HARD_MIX - Hard mix.
  * @property {Number} HUE - Converts the colors to HSL and blends based on hue.
  * @property {Number} INVERT - Overwrites the base color with the inverted new color.
  * @property {Number} INVERT_RGB - Multiplies the new color with the inverted base color.
- * @property {Number} LIGHTEN - Prioritize lighter colors.
+ * @property {Number} LIGHTEN - Prioritize lighter colors. Supports HDR.
  * @property {Number} LINEAR_BURN - Linear burn.
  * @property {Number} LINEAR_DODGE - Same as ADD but limits the result to 1.
  * @property {Number} LINEAR_LIGHT - Linear light.
  * @property {Number} LUMINOSITY - Converts the colors to HSL and blends based on luminosity.
- * @property {Number} MULTIPLY - Color multiplication.
+ * @property {Number} MULTIPLY - Color multiplication. Supports HDR.
  * @property {Number} NEGATION - Negates the base color using the new color.
- * @property {Number} NORMAL - Overwrites the base color with the new one.
+ * @property {Number} NORMAL - Overwrites the base color with the new one. Supports HDR.
  * @property {Number} OVERLAY - Color overlay.
  * @property {Number} PIN_LIGHT - Pin light.
  * @property {Number} REFLECT - Color reflection.
  * @property {Number} SCREEN - Screen blending. The two colors are effectively projected on a white screen simultaneously.
- * @property {Number} SRC - Overwrites the base color with the new one. Ignores opacity.
+ * @property {Number} SRC - Overwrites the base color with the new one while ignoring opacity. Supports HDR.
  * @property {Number} SATURATION - Converts the colors to HSL and blends based on saturation.
  * @property {Number} SOFT_LIGHT - Soft light.
  * @property {Number} SUBTRACT - Subtracts the new color from the base color.
