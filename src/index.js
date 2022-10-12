6724442f34bf033e965b6f2a688132d304de7584
Raoul v. R <vanruesc@users.noreply.github.com>
2022-10-12 16:49:28 +0200

    Move export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export * from "./core";
 export * from "./enums";
 export * from "./effects";
-export { TiltShiftEffect } from "./effects/TiltShiftEffect.js";
 export * from "./loaders";
 export * from "./materials";
 export * from "./passes";
