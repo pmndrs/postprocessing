822cf22b40c44c273b9261070f792c2c7a12ff13
Raoul v. R <vanruesc@users.noreply.github.com>
2023-10-06 23:58:15 +0200

    Update GBuffer.ts

--- src/enums/GBuffer.ts
+++ src/enums/GBuffer.ts
@@ -1,17 +1,41 @@
 /**
  * An enumeration of G-Buffer components.
  *
+ * The string values of the enum constants are used as shader macros.
+ *
  * @group Enums
  */
 
 export enum GBuffer {
-	COLOR = "gbuffer.color",
-	DEPTH = "gbuffer.depth",
-	POSITION = "gbuffer.position",
-	NORMAL = "gbuffer.normal",
-	NORMAL_DEPTH = "gbuffer.normaldepth",
-	VELOCITY = "gbuffer.velocity",
-	LUMINANCE = "gbuffer.luminance",
-	ROUGHNESS = "gbuffer.roughness",
-	METALNESS = "gbuffer.metalness"
+
+	/**
+	 * The albedo/diffuse color of the scene. Takes up one buffer, either 8 or 16 bit RGBA.
+	 */
+
+	COLOR = "GBUFFER_COLOR",
+
+	/**
+	 * The scene depth is stored in a 24 or 32 bit depth texture.
+	 */
+
+	DEPTH = "GBUFFER_DEPTH",
+
+	/**
+	 * The screen-space normals are stored in a 16-bit RGB buffer.
+	 */
+
+	NORMAL = "GBUFFER_NORMAL",
+
+	/**
+	 * Roughness is stored in the GREEN channel of an 8 bit RG buffer.
+	 */
+
+	METALNESS = "GBUFFER_METALNESS",
+
+	/**
+	 * Roughness is stored in the RED channel of an 8 bit RG buffer.
+	 */
+
+	ROUGHNESS = "GBUFFER_ROUGHNESS"
+
 }
