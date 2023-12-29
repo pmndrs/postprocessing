6dc8ba8a3c1a1d6c676f274d3645e6dd8190fa05
Raoul v. R <vanruesc@users.noreply.github.com>
2023-12-29 02:21:28 +0100

    Update EffectShaderSection.ts

--- src/enums/EffectShaderSection.ts
+++ src/enums/EffectShaderSection.ts
@@ -10,30 +10,48 @@ export enum EffectShaderSection {
 	 * A placeholder for function and variable declarations in the fragment shader.
 	 */
 
-	FRAGMENT_HEAD = "FRAGMENT_HEAD",
+	FRAGMENT_HEAD = "$FRAGMENT_HEAD",
+
+	/**
+	 * A placeholder for GBuffer declarations in the fragment shader.
+	 */
+
+	FRAGMENT_HEAD_GBUFFER = "$FRAGMENT_HEAD_GBUFFER",
+
+	/**
+	 * A placeholder for GData declarations in the fragment shader.
+	 */
+
+	FRAGMENT_HEAD_GDATA = "$FRAGMENT_HEAD_GDATA",
 
 	/**
 	 * A placeholder for UV transformations in the fragment shader.
 	 */
 
-	FRAGMENT_MAIN_UV = "FRAGMENT_MAIN_UV",
+	FRAGMENT_MAIN_UV = "$FRAGMENT_MAIN_UV",
+
+	/**
+	 * A placeholder for GData preparations in the fragment shader.
+	 */
+
+	FRAGMENT_MAIN_GDATA = "$FRAGMENT_MAIN_GDATA",
 
 	/**
 	 * A placeholder for color calculations in the fragment shader.
 	 */
 
-	FRAGMENT_MAIN_IMAGE = "FRAGMENT_MAIN_IMAGE",
+	FRAGMENT_MAIN_IMAGE = "$FRAGMENT_MAIN_IMAGE",
 
 	/**
 	 * A placeholder for function and variable declarations in the vertex shader.
 	 */
 
-	VERTEX_HEAD = "VERTEX_HEAD",
+	VERTEX_HEAD = "$VERTEX_HEAD",
 
 	/**
 	 * A placeholder for supporting calculations in the vertex shader.
 	 */
 
-	VERTEX_MAIN_SUPPORT = "VERTEX_MAIN_SUPPORT"
+	VERTEX_MAIN_SUPPORT = "$VERTEX_MAIN_SUPPORT"
 
 }
