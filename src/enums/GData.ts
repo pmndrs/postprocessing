819656cf0c329450f75b660cbad2628b94d4187d
Raoul v. R <vanruesc@users.noreply.github.com>
2023-12-29 02:19:45 +0100

    Create GData

---
+++ src/enums/GData.ts
@@ -0,0 +1,14 @@
+/**
+ * An enumeration of geometry data that is available to Effects.
+ *
+ * @group Enums
+ */
+
+export enum GData {
+	COLOR = "color",
+	DEPTH = "depth",
+	NORMAL = "normal",
+	ROUGHNESS = "roughness",
+	METALNESS = "metalness",
+	LUMINANCE = "luminance"
+}
