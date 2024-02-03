a1501e0ef2de3ab4d6e6172fa0db546f28ff5a06
Raoul v. R <vanruesc@users.noreply.github.com>
2024-02-03 23:23:37 +0100

    Update ToneMappingMode.js
    Closes #567

--- src/enums/ToneMappingMode.js
+++ src/enums/ToneMappingMode.js
@@ -5,8 +5,8 @@
  * @property {Number} REINHARD - Simple Reinhard tone mapping.
  * @property {Number} REINHARD2 - Modified Reinhard tone mapping.
  * @property {Number} REINHARD2_ADAPTIVE - Simulates the optic nerve responding to the amount of light it is receiving.
- * @property {Number} OPTIMIZED_CINEON - Optimized filmic operator by Jim Hejl and Richard Burgess-Dawson.
  * @property {Number} UNCHARTED2 - Uncharted 2 tone mapping. http://filmicworlds.com/blog/filmic-tonemapping-operators.
+ * @property {Number} OPTIMIZED_CINEON - Optimized filmic operator by Jim Hejl and Richard Burgess-Dawson.
  * @property {Number} ACES_FILMIC - ACES tone mapping with a scale of 1.0/0.6.
  * @property {Number} AGX - Filmic tone mapping. Requires three r160 or higher. https://github.com/EaryChow/AgX.
  */
