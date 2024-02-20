5cb35a59f2f53094a77289da2d3a38dcbdfed4f1
Balraj Johal <balrajsjohal@gmail.com>
2024-02-20 21:36:29 +0000

    Alphabetise exports

--- src/effects/index.ts
+++ src/effects/index.ts
@@ -3,6 +3,6 @@ export * from "./blending/index.js";
 export * from "./BloomEffect.js";
 export * from "./Effect.js";
 export * from "./FXAAEffect.js";
+export * from "./ScanlineEffect.js";
 export * from "./ToneMappingEffect.js";
 export * from "./VignetteEffect.js";
-export * from "./ScanlineEffect.js";
