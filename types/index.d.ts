ad2eee635fc8de73cba8b1012c20fd12b343cb0d
Raoul v. R <vanruesc@users.noreply.github.com>
2024-03-02 16:21:01 +0100

    Add LINEAR and NEUTRAL tone mapping

--- types/index.d.ts
+++ types/index.d.ts
@@ -8354,6 +8354,7 @@ declare module "postprocessing" {
 	 * A tone mapping mode enumeration.
 	 *
 	 * @type {Object}
+	 * @property {Number} LINEAR - No tone mapping, only exposure.
 	 * @property {Number} REINHARD - Simple Reinhard tone mapping.
 	 * @property {Number} REINHARD2 - Modified Reinhard tone mapping.
 	 * @property {Number} REINHARD2_ADAPTIVE - Simulates the optic nerve responding to the amount of light it is receiving.
@@ -8361,8 +8362,10 @@ declare module "postprocessing" {
 	 * @property {Number} OPTIMIZED_CINEON - Optimized filmic operator by Jim Hejl and Richard Burgess-Dawson.
 	 * @property {Number} ACES_FILMIC - ACES tone mapping with a scale of 1.0/0.6.
 	 * @property {Number} AGX - Filmic tone mapping. Requires three r160 or higher. https://github.com/EaryChow/AgX.
+	 * @property {Number} NEUTRAL - Neutral tone mapping by Khronos. Requires three r162 or higher. https://modelviewer.dev/examples/tone-mapping.
 	 */
 	export enum ToneMappingMode {
+		LINEAR,
 		REINHARD,
 		REINHARD2,
 		REINHARD2_ADAPTIVE,
@@ -8370,6 +8373,7 @@ declare module "postprocessing" {
 		OPTIMIZED_CINEON,
 		ACES_FILMIC,
 		AGX,
+		NEUTRAL
 	}
 
 	/**
