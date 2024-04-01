d1448256b26826e5bb28d707d038120ed29b2517
Raoul v. R. <vanruesc@users.noreply.github.com>
2024-04-01 16:41:27 +0200

    Add doc comments

--- src/enums/GData.ts
+++ src/enums/GData.ts
@@ -6,10 +6,41 @@
  */
 
 export enum GData {
+
+	/**
+	 * The fragment color.
+	 */
+
 	COLOR = "color",
+
+	/**
+	 * The fragment depth.
+	 */
+
 	DEPTH = "depth",
+
+	/**
+	 * The view-space normal.
+	 */
+
 	NORMAL = "normal",
+
+	/**
+	 * Occlusion, roughness and metalness.
+	 */
+
 	ORM = "orm",
+
+	/**
+	 * The total emissive radiance.
+	 */
+
 	EMISSION = "emission",
+
+	/**
+	 * The fragment luminance.
+	 */
+
 	LUMINANCE = "luminance"
+
 }
