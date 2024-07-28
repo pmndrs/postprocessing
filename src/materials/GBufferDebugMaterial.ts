08cbb9b469ae617158c7e21337f4250d41c49913
Raoul v. R. <vanruesc@users.noreply.github.com>
2024-07-28 23:35:54 +0200

    Create GBufferDebugMaterial

---
+++ src/materials/GBufferDebugMaterial.ts
@@ -0,0 +1,108 @@
+import { Texture, Uniform } from "three";
+import { FullscreenMaterial } from "./FullscreenMaterial.js";
+
+import fragmentShader from "./shaders/gbuffer-debug.frag";
+import vertexShader from "./shaders/common.vert";
+
+/**
+ * A G-Buffer debug shader material.
+ *
+ * @category Materials
+ */
+
+export class GBufferDebugMaterial extends FullscreenMaterial {
+
+	/**
+	 * Constructs a new G-Buffer debug material.
+	 */
+
+	constructor() {
+
+		super({
+			name: "GBufferDebugMaterial",
+			fragmentShader,
+			vertexShader,
+			uniforms: {
+				depthBuffer: new Uniform(null)
+			}
+		});
+
+	}
+
+	/**
+	 * A depth buffer.
+	 */
+
+	get depthBuffer(): Texture | null {
+
+		return this.uniforms.depthBuffer.value as Texture;
+
+	}
+
+	set depthBuffer(value: Texture | null) {
+
+		this.uniforms.depthBuffer.value = value;
+
+	}
+
+	/**
+	 * Enables or disables view space position reconstruction.
+	 */
+
+	get reconstructPosition(): boolean {
+
+		return (this.defines.RECONSTRUCT_POSITION !== undefined);
+
+	}
+
+	set reconstructPosition(value: boolean) {
+
+		if(this.reconstructPosition !== value) {
+
+			if(value) {
+
+				this.defines.RECONSTRUCT_POSITION = true;
+
+			} else {
+
+				delete this.defines.RECONSTRUCT_POSITION;
+
+			}
+
+			this.needsUpdate = true;
+
+		}
+
+	}
+
+	/**
+	 * Indicates whether output color space conversion is enabled.
+	 */
+
+	get colorSpaceConversion(): boolean {
+
+		return (this.defines.COLOR_SPACE_CONVERSION !== undefined);
+
+	}
+
+	set colorSpaceConversion(value: boolean) {
+
+		if(this.colorSpaceConversion !== value) {
+
+			if(value) {
+
+				this.defines.COLOR_SPACE_CONVERSION = true;
+
+			} else {
+
+				delete this.defines.COLOR_SPACE_CONVERSION;
+
+			}
+
+			this.needsUpdate = true;
+
+		}
+
+	}
+
+}
---
+++ src/materials/shaders/gbuffer-debug.frag
@@ -0,0 +1,36 @@
+#include <common>
+#include <dithering_pars_fragment>
+#include <packing>
+
+#include <pp_camera_pars_fragment>
+#include <pp_default_output_pars_fragment>
+#include <pp_input_buffer_pars_fragment>
+#include <pp_depth_buffer_pars_fragment>
+#include <pp_depth_utils_pars_fragment>
+#include <pp_world_utils_pars_fragment>
+
+in vec2 vUv;
+
+void main() {
+
+	#ifdef RECONSTRUCT_POSITION
+
+		float depth = readDepth(depthBuffer, vUv);
+		vec3 viewPosition = getViewPosition(vUv, depth);
+		out_Color = vec4(getWorldPosition(viewPosition), 1.0);
+
+	#else
+
+		out_Color = texture(inputBuffer, vUv);
+
+		#ifdef COLOR_SPACE_CONVERSION
+
+			#include <colorspace_fragment>
+
+		#endif
+
+		#include <dithering_fragment>
+
+	#endif
+
+}
