7084a5fe0ee3312dfe248e5e2135bc401859b543
Raoul v. R. <vanruesc@users.noreply.github.com>
2024-07-29 00:21:39 +0200

    Update GeometryPass.js

--- test/passes/GeometryPass.js
+++ test/passes/GeometryPass.js
@@ -1,9 +1,10 @@
 import test from "ava";
+import { PerspectiveCamera, Scene } from "three";
 import { GeometryPass } from "postprocessing";
 
 test("can be created and destroyed", t => {
 
-	const object = new GeometryPass();
+	const object = new GeometryPass(new Scene(), new PerspectiveCamera());
 	object.dispose();
 
 	t.pass();
