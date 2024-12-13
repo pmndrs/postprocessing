7ba3bce42cdeae34eef669af0bba48ba33383b34
Raoul v. R. <vanruesc@outlook.de>
2024-12-13 14:23:22 +0100

    Create footer.ts

---
+++ manual/assets/js/src/inline/footer.ts
@@ -0,0 +1,58 @@
+/**
+ * Restores the menu expansion state.
+ */
+
+function restoreMenuState(): void {
+
+	const expandedMenusJSON = sessionStorage.getItem("expanded-menus");
+	const expandedMenus = (expandedMenusJSON !== null) ? JSON.parse(expandedMenusJSON) as string[] : null;
+
+	if(expandedMenus === null || !Array.isArray(expandedMenus) || expandedMenus.length === 0) {
+
+		return;
+
+	}
+
+	for(const menu of document.querySelectorAll("nav .menu")) {
+
+		menu.classList.remove("expanded");
+
+	}
+
+	try {
+
+		for(const menu of document.querySelectorAll(expandedMenus.map(id => `.menu.${id}`).join(","))) {
+
+			menu.classList.add("expanded");
+
+		}
+
+	} catch(e) {
+
+		sessionStorage.removeItem("expanded-menus");
+		window.location.reload();
+
+	}
+
+}
+
+/**
+ * Restores scroll positions.
+ */
+
+function restoreScroll(): void {
+
+	const nav = document.querySelector(".navigation");
+	const main = document.querySelector("main");
+
+	if(nav !== null && main !== null) {
+
+		nav.scrollTop = Number(sessionStorage.getItem("nav-scroll"));
+		main.scrollTop = Number(sessionStorage.getItem("main-scroll"));
+
+	}
+
+}
+
+restoreMenuState();
+restoreScroll();
