279e6c1c25bd3a55531dfcd1d57e60ab078ffcf4
Raoul v. R. <vanruesc@outlook.de>
2024-12-26 00:29:05 +0100

    Use copyTextureToTexture

--- src/passes/CopyPass.ts
+++ src/passes/CopyPass.ts
@@ -1,4 +1,4 @@
-import { WebGLRenderTarget } from "three";
+import { WebGLRenderer, WebGLRenderTarget } from "three";
 import { Pass } from "../core/Pass.js";
 import { GBuffer } from "../enums/GBuffer.js";
 import { CopyMaterial } from "../materials/CopyMaterial.js";
@@ -26,16 +26,117 @@ export class CopyPass extends Pass<CopyMaterial> {
 
 	}
 
+	override get renderer(): WebGLRenderer | null {
+
+		return super.renderer;
+
+	}
+
+	override set renderer(value: WebGLRenderer | null) {
+
+		super.renderer = value;
+		this.initializeOutputBuffer();
+
+	}
+
+	/**
+	 * Initializes the current output buffer.
+	 */
+
+	private initializeOutputBuffer(): void {
+
+		const outputBuffer = this.output.defaultBuffer?.value ?? null;
+
+		if(this.renderer !== null && outputBuffer !== null) {
+
+			this.renderer.initRenderTarget(outputBuffer);
+
+		}
+
+	}
+
 	protected override onInputChange(): void {
 
 		this.fullscreenMaterial.depthBuffer = this.input.getBuffer(GBuffer.DEPTH);
 
 	}
 
+	protected override onOutputChange(): void {
+
+		this.initializeOutputBuffer();
+
+	}
+
+	protected override onResolutionChange(): void {
+
+		this.initializeOutputBuffer();
+
+	}
+
+	/**
+	 * Rapidly copies the input buffer into the output buffer.
+	 *
+	 * @return True, if the operation was successful.
+	 */
+
+	private blit(): boolean {
+
+		const inputBuffer = this.input.defaultBuffer?.value ?? null;
+		const outputBuffer = this.output.defaultBuffer?.value ?? null;
+
+		if(this.renderer === null || inputBuffer === null || outputBuffer === null) {
+
+			return false;
+
+		}
+
+		const imgData = inputBuffer.source.data as ImageData;
+
+		if(imgData.width !== outputBuffer.width || imgData.height !== outputBuffer.height) {
+
+			return false;
+
+		}
+
+		if(inputBuffer.type === outputBuffer.texture.type &&
+			inputBuffer.format === outputBuffer.texture.format) {
+
+			this.renderer.copyTextureToTexture(inputBuffer, outputBuffer.texture);
+
+		}
+
+		const inputDepthTexture = this.input.getBuffer(GBuffer.DEPTH);
+
+		if(inputDepthTexture !== null && outputBuffer.depthTexture !== null &&
+			inputDepthTexture !== outputBuffer.depthTexture &&
+			inputDepthTexture.type === outputBuffer.depthTexture.type &&
+			inputDepthTexture.format === outputBuffer.depthTexture.format) {
+
+			this.renderer.copyTextureToTexture(inputDepthTexture, outputBuffer.depthTexture);
+
+		}
+
+		return true;
+
+	}
+
 	override render(): void {
 
-		this.renderer?.setRenderTarget(this.output.defaultBuffer?.value ?? null);
-		this.renderFullscreen();
+		const inputBuffer = this.input.defaultBuffer?.value ?? null;
+
+		if(this.renderer === null || inputBuffer === null) {
+
+			return;
+
+		}
+
+		if(!this.blit()) {
+
+			// Blit failed: use a shader to copy the input buffer.
+			this.setRenderTarget(this.output.defaultBuffer?.value ?? null);
+			this.renderFullscreen();
+
+		}
 
 	}
 
