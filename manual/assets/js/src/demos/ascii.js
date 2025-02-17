46ff2b05df18632e1c085ba15169354edcf71ecb
Raoul v. R. <vanruesc@outlook.de>
2025-02-17 01:21:59 +0100

    Update ascii.js
    Addresses #683

--- manual/assets/js/src/demos/ascii.js
+++ manual/assets/js/src/demos/ascii.js
@@ -1,18 +1,20 @@
 import {
-    CubeTextureLoader,
-    FogExp2,
-    LoadingManager,
-    PerspectiveCamera,
-    Scene,
-    SRGBColorSpace,
-    WebGLRenderer
+	CubeTextureLoader,
+	FogExp2,
+	LoadingManager,
+	PerspectiveCamera,
+	Scene,
+	SRGBColorSpace,
+	WebGLRenderer
 } from "three";
 
 import {
-    AsciiEffect,
-    EffectComposer,
-    EffectPass,
-    RenderPass
+	ASCIIEffect,
+	ASCIITexture,
+	BlendFunction,
+	EffectComposer,
+	EffectPass,
+	RenderPass
 } from "postprocessing";
 
 import { Pane } from "tweakpane";
@@ -22,117 +24,132 @@ import * as Domain from "../objects/Domain";
 
 function load() {
 
-    const assets = new Map();
-    const loadingManager = new LoadingManager();
-    const cubeTextureLoader = new CubeTextureLoader(loadingManager);
+	const assets = new Map();
+	const loadingManager = new LoadingManager();
+	const cubeTextureLoader = new CubeTextureLoader(loadingManager);
 
-    const path = document.baseURI + "img/textures/skies/sunset/";
-    const format = ".png";
-    const urls = [
-        path + "px" + format, path + "nx" + format,
-        path + "py" + format, path + "ny" + format,
-        path + "pz" + format, path + "nz" + format
-    ];
+	const path = document.baseURI + "img/textures/skies/sunset/";
+	const format = ".png";
+	const urls = [
+		path + "px" + format, path + "nx" + format,
+		path + "py" + format, path + "ny" + format,
+		path + "pz" + format, path + "nz" + format
+	];
 
-    return new Promise((resolve, reject) => {
+	return new Promise((resolve, reject) => {
 
-        loadingManager.onLoad = () => resolve(assets);
-        loadingManager.onError = (url) => reject(new Error(`Failed to load ${url}`));
+		loadingManager.onLoad = () => resolve(assets);
+		loadingManager.onError = (url) => reject(new Error(`Failed to load ${url}`));
 
-        cubeTextureLoader.load(urls, (t) => {
+		cubeTextureLoader.load(urls, (t) => {
 
-            t.colorSpace = SRGBColorSpace;
-            assets.set("sky", t);
+			t.colorSpace = SRGBColorSpace;
+			assets.set("sky", t);
 
-        });
+		});
 
-    });
+	});
 
 }
 
 window.addEventListener("load", () => load().then((assets) => {
 
-    // Renderer
+	// Renderer
 
-    const renderer = new WebGLRenderer({
-        powerPreference: "high-performance",
-        antialias: false,
-        stencil: false,
-        depth: false
-    });
+	const renderer = new WebGLRenderer({
+		powerPreference: "high-performance",
+		antialias: false,
+		stencil: false,
+		depth: false
+	});
 
-    renderer.debug.checkShaderErrors = (window.location.hostname === "localhost");
-    const container = document.querySelector(".viewport");
-    container.prepend(renderer.domElement);
+	renderer.debug.checkShaderErrors = (window.location.hostname === "localhost");
+	const container = document.querySelector(".viewport");
+	container.prepend(renderer.domElement);
 
-    // Camera & Controls
+	// Camera & Controls
 
-    const camera = new PerspectiveCamera();
-    const controls = new SpatialControls(camera.position, camera.quaternion, renderer.domElement);
-    const settings = controls.settings;
-    settings.rotation.sensitivity = 2.2;
-    settings.rotation.damping = 0.05;
-    settings.translation.damping = 0.1;
-    controls.position.set(0, 0, 1);
-    controls.lookAt(0, 0, 0);
+	const camera = new PerspectiveCamera();
+	const controls = new SpatialControls(camera.position, camera.quaternion, renderer.domElement);
+	const settings = controls.settings;
+	settings.rotation.sensitivity = 2.2;
+	settings.rotation.damping = 0.05;
+	settings.translation.damping = 0.1;
+	controls.position.set(0, 10, 1);
+	controls.lookAt(0, 10, -1);
 
-    // Scene, Lights, Objects
+	// Scene, Lights, Objects
 
-    const scene = new Scene();
-    scene.fog = new FogExp2(0x373134, 0.06);
-    scene.background = assets.get("sky");
-    scene.add(Domain.createLights());
+	const scene = new Scene();
+	scene.fog = new FogExp2(0x373134, 0.06);
+	scene.background = assets.get("sky");
+	scene.add(Domain.createLights());
+	scene.add(Domain.createEnvironment(scene.background));
+	scene.add(Domain.createActors(scene.background));
 
-    // Post Processing
+	// Post Processing
 
-    const composer = new EffectComposer(renderer);
+	const composer = new EffectComposer(renderer, {
+		multisampling: Math.min(4, renderer.capabilities.maxSamples)
+	});
 
-    const effect = new AsciiEffect({
-        font: "arial",
-        characters: ` .:,'-^=*+?!|0#X%WM@`,
-        fontSize: 35,
-        cellSize: 16,
-        color: "#ffffff",
-        invert: false,
-        sceneColor: true
-    });
-    composer.addPass(new RenderPass(scene, camera));
-    composer.addPass(new EffectPass(camera, effect));
+	const effect = new ASCIIEffect({
+		asciiTexture: new ASCIITexture({
+			characters: " .:,'-^=*+?!|0#X%WM@",
+			font: "Arial",
+			fontSize: 54,
+			size: 1024,
+			maxCharsPerRow: 16
+		}),
+		cellSize: 12,
+		inverted: false
+	});
 
-    // Settings
+	composer.addPass(new RenderPass(scene, camera));
+	composer.addPass(new EffectPass(camera, effect));
 
-    const fpsMeter = new FPSMeter();
-    const pane = new Pane({ container: container.querySelector(".tp") });
-    pane.addBinding(fpsMeter, "fps", { readonly: true, label: "FPS" });
+	// Settings
 
-    const folder = pane.addFolder({ title: "Settings" });
-    // folder.addBinding(effect, "granularity", { min: 0, max: 20, step: 1 });
+	const params = { useSceneColor: true };
 
+	const fpsMeter = new FPSMeter();
+	const pane = new Pane({ container: container.querySelector(".tp") });
+	pane.addBinding(fpsMeter, "fps", { readonly: true, label: "FPS" });
 
-    // Resize Handler
+	const folder = pane.addFolder({ title: "Settings" });
+	folder.addBinding(effect, "inverted");
+	folder.addBinding(effect, "cellSize", { min: 2, max: 24, step: 2 });
+	folder.addBinding(effect, "color", { color: { type: "float" } });
+	folder.addBinding(params, "useSceneColor").on("change",
+		(e) => void (effect.color = e.value ? null : effect.color.getHex()));
 
-    function onResize() {
+	folder.addBinding(effect.blendMode.opacity, "value", { label: "opacity", min: 0, max: 1, step: 0.01 });
+	folder.addBinding(effect.blendMode, "blendFunction", { options: BlendFunction });
 
-        const width = container.clientWidth, height = container.clientHeight;
-        camera.aspect = width / height;
-        camera.fov = calculateVerticalFoV(90, Math.max(camera.aspect, 16 / 9));
-        camera.updateProjectionMatrix();
-        composer.setSize(width, height);
+	// Resize Handler
 
-    }
+	function onResize() {
 
-    window.addEventListener("resize", onResize);
-    onResize();
+		const width = container.clientWidth, height = container.clientHeight;
+		camera.aspect = width / height;
+		camera.fov = calculateVerticalFoV(90, Math.max(camera.aspect, 16 / 9));
+		camera.updateProjectionMatrix();
+		composer.setSize(width, height);
 
-    // Render Loop
+	}
 
-    requestAnimationFrame(function render(timestamp) {
+	window.addEventListener("resize", onResize);
+	onResize();
 
-        fpsMeter.update(timestamp);
-        controls.update(timestamp);
-        composer.render();
-        requestAnimationFrame(render);
+	// Render Loop
 
-    });
+	requestAnimationFrame(function render(timestamp) {
+
+		fpsMeter.update(timestamp);
+		controls.update(timestamp);
+		composer.render();
+		requestAnimationFrame(render);
+
+	});
 
 }));
