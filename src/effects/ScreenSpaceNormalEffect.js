3d90dcc5cdf83368bce7e555926741f752ae0a5f
Jonathan Chemla <jos.chemla@gmail.com>
2025-08-11 15:55:55 +0200

    Cleanup ScreenSpaceNormalEffect

--- src/effects/ScreenSpaceNormalEffect.js
+++ src/effects/ScreenSpaceNormalEffect.js
@@ -18,32 +18,14 @@ export class ScreenSpaceNormalEffect extends Effect {
 				uniform sampler2D maskTexture;
 				uniform mat3 camera_normalMatrix;
 				uniform mat4 camera_projectionMatrixInverse;
-				// uniform vec3 test;
-				// uniform float testt;
-				// uniform mat3 camera_normalMatrix_inv;
-				// uniform mat4 camera_modelViewMatrix;
-				// uniform mat4 camera_modelViewMatrix_inv;
-				// uniform mat4 camera_matrixWorldInverse;
-				// uniform mat4 camera_matrixWorld;
-				
-				
-				// float readDepth(const in vec2 uv). To calculate the view Z based on depth, you can use the predefined function float getViewZ(const in float depth)
-				// mat4 normalMatrix = transpose(inverse(modelView));
-				// float d = readDepth(uv);
-				// vec3 uvd = vec3(uv*0.5 - 0.5, d);
-				// vec3 xyz = camera_normalMatrix * uvd;
-				// vec2 texel = vec2( 1.0 / resolution.x, 1.0 / resolution.y );
-				// float tx0y0 = texture2D( inputBuffer, uv + texel * vec2( -1, -1 ) ).r;
 				
 				vec3 get_camera_space_coords_mat3 (const in vec2 uv_co) {
 					float d = readDepth(uv_co); //  * 2. - 1.;
 					vec3 uvd = vec3(uv_co * 2. - 1., d);
-					// vec3 uvd = vec3(uv_co * 0.5 - 0.5, readDepth(uv_co));
-					// vec3 uvd = vec3(uv_co, readDepth(uv_co));
 					vec3 xyz = camera_normalMatrix * uvd;
 					return xyz;
 				}
-				// Working?
+
 				vec3 get_camera_space_coords_mat4 (const in vec2 uv_co) {
 					float d = readDepth(uv_co) * 2. - 1.;
 					vec4 uvd = vec4(uv_co * 2. - 1., d, 1.);
@@ -52,7 +34,6 @@ export class ScreenSpaceNormalEffect extends Effect {
 				}
 
 				vec3 get_camera_space_coords (const in vec2 uv_co) {
-					// return get_camera_space_coords_mat3(uv_co);
 					return get_camera_space_coords_mat4(uv_co);
 				}
 
@@ -117,34 +98,17 @@ export class ScreenSpaceNormalEffect extends Effect {
 					
 					float depth = readDepth(uv_co); // c0
 
-
-					
-					// float dl = abs(l1*l2/(2.0*l2-l1)-c0);
-					// float dr = abs(r1*r2/(2.0*r2-r1)-c0);
-					// float db = abs(b1*b2/(2.0*b2-b1)-c0);
-					// float dt = abs(t1*t2/(2.0*t2-t1)-c0);
-					// vec3 dpdx = (dl<dr) ?  get_camera_space_coords ( uv_co )- get_camera_space_coords ( uv_co + texelSize * vec2(-1., 0.) ) : 
-					// 					- get_camera_space_coords ( uv_co )+ get_camera_space_coords ( uv_co + texelSize * vec2(+1., 0.) ) ;
-
 					vec4 H;
 					H.x = readDepth(uv_co + texelSize * vec2(-1., 0.)); // L1
 					H.y = readDepth(uv_co + texelSize * vec2(+1., 0.)); // R1
 					H.z = readDepth(uv_co + texelSize * vec2(-2., 0.)); // L2
 					H.w = readDepth(uv_co + texelSize * vec2(+2., 0.)); // R2
-					// DL = abs(l1*l2/(2.0*l2-l1)-c0);
-					// DL = abs(H.x * H.z / (2.0 * H.z - H.x) - depth);
-					// DR = abs(r1*r2/(2.0*r2-r1)-c0);
-					// DR = abs(H.y * H.w / (2.0 * H.w - H.y) - depth);
+
 					vec2 he = abs(H.xy * H.zw / (2. * H.zw - H.xy) - depth);
 					vec3 hDeriv;
-					// if (he.x <= he.y) 
-					// if (abs(he.x - he.y) > 0.0001) 
 					if (he.x > he.y) 
-						// hDeriv = Calculate horizontal derivative of world position from taps | * | y |
-						// hDeriv = posY - posCenter;
 						hDeriv = get_camera_space_coords ( uv_co ) - get_camera_space_coords ( uv_co + texelSize * vec2(-1., 0.) );
 					else
-						// hDeriv = Calculate horizontal derivative of world position from taps | x | * |
 						hDeriv = - get_camera_space_coords ( uv_co ) + get_camera_space_coords ( uv_co + texelSize * vec2(+1., 0.) );
 
 					vec4 V;
@@ -154,16 +118,12 @@ export class ScreenSpaceNormalEffect extends Effect {
 					V.w = readDepth(uv_co + texelSize * vec2( 0., +2.));
 					vec2 ve = abs(V.xy * V.zw / (2. * V.zw - V.xy) - depth);
 					vec3 vDeriv;
-					// if (ve.x > ve.y)
 					if (abs(ve.y - ve.x) > 0.0001)
-						// vDeriv = Calculate vertical derivative of world position from taps | * | y |
 						hDeriv = get_camera_space_coords (uv_co + texelSize * vec2(0., +1.)) - get_camera_space_coords ( uv_co );
 					else
-						// vDeriv = Calculate vertical derivative of world position from taps | x | * |
 						vDeriv = get_camera_space_coords ( uv_co ) - get_camera_space_coords ( uv_co + texelSize * vec2(0., -1.) );
 
 					return normalize(cross(hDeriv, vDeriv));
-					// return normalize(hDeriv);
 				}
 
 				vec3 get_normal_accurate_iq(const in vec2 uv_co) {
@@ -202,7 +162,6 @@ export class ScreenSpaceNormalEffect extends Effect {
 										-ce +get_camera_space_coords ( uv_co + texelSize * vec2(0., +1.) ) ;
 
 					return normalize(cross(dpdx,dpdy));
-					// return normalize(dpdx);
 				}
 
 
