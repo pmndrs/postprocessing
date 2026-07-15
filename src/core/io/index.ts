31079b578c72d6a04a07921bd0725578a335dffa
Raoul v. R. <vanruesc@outlook.de>
2026-07-16 00:14:08 +0200

    Update index.ts

--- src/core/io/index.ts
+++ src/core/io/index.ts
@@ -1,7 +1,10 @@
 export * from "./DisposableResource.js";
+export * from "./GBufferResource.js";
 export * from "./Input.js";
-export * from "./IOManager.js";
 export * from "./Output.js";
 export * from "./RenderTargetResource.js";
 export * from "./Resource.js";
+export * from "./ResourceManager.js";
+export * from "./ShaderDataResource.js";
 export * from "./TextureResource.js";
+
