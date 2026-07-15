78cea2bbc498f8bfbda3d4721a1a7b6158772722
Raoul v. R. <vanruesc@outlook.de>
2026-07-16 00:14:10 +0200

    Update index.ts

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,10 +2,11 @@ export * from "./io/index.js";
 
 export * from "./BaseEventMap.js";
 export * from "./Disposable.js";
+export * from "./FrameGraph.js";
 export * from "./Identifiable.js";
 export * from "./Pass.js";
 export * from "./Renderable.js";
+export * from "./RenderTask.js";
 export * from "./Resizable.js";
-export * from "./RenderPipeline.js";
 export * from "./Selective.js";
-export * from "./ShaderData.js";
+export * from "./Task.js";
