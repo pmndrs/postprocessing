027f373f155d30b49d61f55559f6e827bc7c8556
Raoul v. R. <vanruesc@outlook.de>
2026-07-16 00:11:28 +0200

    Create RenderTask.ts

---
+++ src/core/RenderTask.ts
@@ -0,0 +1,138 @@
+import { BaseEvent, Camera, EventDispatcher, Scene, WebGLRenderer } from "three";
+import { ReadonlyTimer } from "../utils/ReadonlyTimer.js";
+import { Resolution } from "../utils/Resolution.js";
+import { Scissor } from "../utils/Scissor.js";
+import { Viewport } from "../utils/Viewport.js";
+import { Disposable } from "./Disposable.js";
+import { Identifiable } from "./Identifiable.js";
+import { Input } from "./io/Input.js";
+import { Output } from "./io/Output.js";
+import { Renderable } from "./Renderable.js";
+import { Task } from "./Task.js";
+import { BaseEventMap } from "./BaseEventMap.js";
+
+/**
+ * RenderTask events.
+ *
+ * @category Core
+ */
+
+export interface RenderTaskEventMap extends BaseEventMap {
+
+	/**
+	 * Triggers when the render task gets enabled or disabled.
+	 *
+	 * @event
+	 */
+
+	toggle: BaseEvent<"toggle">;
+
+}
+
+/**
+ * A render task.
+ *
+ * @category Core
+ */
+
+export interface RenderTask extends EventDispatcher<RenderTaskEventMap>, Disposable, Identifiable, Renderable, Task {
+
+	/**
+	 * The input resources of this task.
+	 */
+
+	readonly input: Input;
+
+	/**
+	 * The output resources of this task.
+	 */
+
+	readonly output: Output;
+
+	/**
+	 * The current resolution.
+	 */
+
+	readonly resolution: Resolution;
+
+	/**
+	 * The viewport.
+	 *
+	 * @see {@link Viewport.enabled} to enable the viewport.
+	 */
+
+	readonly viewport: Viewport;
+
+	/**
+	 * A rectangular area inside the viewport. Fragments outside this area will not be rendered.
+	 *
+	 * @see {@link Scissor.enabled} to enable the scissor.
+	 */
+
+	readonly scissor: Scissor;
+
+	/**
+	 * A timer.
+	 */
+
+	timer: ReadonlyTimer | null;
+
+	/**
+	 * The current renderer.
+	 */
+
+	renderer: WebGLRenderer | null;
+
+	// #region Internal
+
+	/**
+	 * The main scene.
+	 *
+	 * @internal
+	 */
+
+	set mainScene(value: Scene | null);
+
+	/**
+	 * The main camera.
+	 *
+	 * @internal
+	 */
+
+	set mainCamera(value: Camera | null);
+
+	/**
+	 * A list of subtasks.
+	 *
+	 * @internal
+	 */
+
+	readonly subtasks: readonly RenderTask[];
+
+	// #endregion
+
+	/**
+	 * The current scene.
+	 *
+	 * Defaults to the main scene of the associated frame graph if not defined.
+	 */
+
+	scene: Scene | null;
+
+	/**
+	 * The current camera.
+	 *
+	 * Defaults to the main camera of the associated frame graph if not defined.
+	 */
+
+	camera: Camera | null;
+
+	/**
+	 * Compiles the resources used by this task.
+	 *
+	 * @return A promise that resolves when the compilation has finished.
+	 */
+
+	compile(): Promise<void>;
+
+}
