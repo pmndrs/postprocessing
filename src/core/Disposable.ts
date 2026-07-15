ad59177238eb04f65a55fc276582e11d174cea1f
Raoul v. R. <vanruesc@outlook.de>
2026-07-16 00:07:37 +0200

    Add isDisposable function

--- src/core/Disposable.ts
+++ src/core/Disposable.ts
@@ -13,3 +13,17 @@ export interface Disposable {
 	dispose(): void;
 
 }
+
+/**
+ * Checks if a given object can be disposed.
+ *
+ * @param object - The object.
+ * @return Whether the object is of type Disposable.
+ */
+
+export function isDisposable(object?: object): object is Disposable {
+
+	return object !== null && typeof object === "object" &&
+		"dispose" in object && typeof object.dispose === "function";
+
+}
