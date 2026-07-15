a8f07ee8a6bf13a8b08e7961be95329a1c9f2926
Raoul v. R. <vanruesc@outlook.de>
2026-07-16 00:10:57 +0200

    Update ResourceManager.ts

--- src/core/io/ResourceManager.ts
+++ src/core/io/ResourceManager.ts
@@ -1,8 +1,10 @@
-import { Material } from "three";
+import { Material, WebGLRenderTarget } from "three";
 import { Pass } from "../Pass.js";
-import { RenderPipeline } from "../RenderPipeline.js";
-import { DisposableResource } from "./DisposableResource.js";
+import { FrameGraph } from "../FrameGraph.js";
 import { Resource } from "./Resource.js";
+import { isDisposable } from "../Disposable.js";
+import { RenderTargetResource } from "./RenderTargetResource.js";
+import { RenderTargetDescriptor } from "../../utils/RenderTargetDescriptor.js";
 
 /**
  * Gathers all resources from a given pass and its subpasses.
@@ -45,22 +47,29 @@ export class ResourceManager {
 	 * A collection of active render pipelines.
 	 */
 
-	private readonly pipelines: Set<RenderPipeline>;
+	private readonly frameGraphs: Set<FrameGraph>;
 
 	/**
-	 * A set of resources that are currently being used in the render pipelines.
+	 * A set of resources that are currently being used in frame graphs.
 	 */
 
 	private activeResources: Set<Resource>;
 
 	/**
+	 * Indicates whether this manager is currently updating resources.
+	 */
+
+	private updating: boolean;
+
+	/**
 	 * Constructs a new resource manager.
 	 */
 
-	constructor(pipelines: Set<RenderPipeline>) {
+	constructor() {
 
-		this.pipelines = pipelines;
-		this.activeResources = new Set<Resource>();
+		this.frameGraphs = new Set();
+		this.activeResources = new Set();
+		this.updating = false;
 
 	}
 
@@ -74,17 +83,101 @@ export class ResourceManager {
 
 		const result = new Set<Resource>();
 
-		for(const pipeline of this.pipelines) {
+		//for(const graph of this.graphs) {
 
-			for(const pass of pipeline.passes) {
+		//	for(const pass of graph.passes) {
 
-				gatherResources(pass, result);
+		//		gatherResources(pass, result);
 
-			}
+		//	}
+
+		//}
+
+		return result;
+
+	}
+
+	/**
+	 * Creates a new render target based on the given descriptor.
+	 *
+	 * @param descriptor - A render target descriptor.
+	 * @return The new render target.
+	 */
+
+	private createRenderTarget(descriptor: RenderTargetDescriptor): WebGLRenderTarget {
+
+		const renderTarget = new WebGLRenderTarget(1, 1, descriptor);
+		const textureConfigs = Array.from(descriptor.textures.entries());
+
+		for(let i = 0, l = textureConfigs.length; i < l; ++i) {
+
+			const texture = renderTarget.textures[i];
+			const textureConfig = textureConfigs[i];
+			texture.name = textureConfig[0];
+			texture.setValues(textureConfig[1]);
 
 		}
 
-		return result;
+		return renderTarget;
+
+	}
+
+	/**
+	 * Adds a frame graph.
+	 *
+	 * @param graph - The graph to add.
+	 */
+
+	addFrameGraph(graph: FrameGraph): void {
+
+		this.frameGraphs.add(graph);
+
+	}
+
+	/**
+	 * Removes a frame graph.
+	 *
+	 * @param graph - The graph to remove.
+	 */
+
+	removeFrameGraph(graph: FrameGraph): void {
+
+		this.frameGraphs.delete(graph);
+
+	}
+
+	/**
+	 * Updates the input and output resources of all graphs.
+	 */
+
+	update(): void {
+
+		if(this.updating) {
+
+			return;
+
+		}
+
+		this.updating = true;
+
+		for(const graph of this.frameGraphs) {
+
+			this.updateFrameGraph(graph);
+
+		}
+
+		this.optimize();
+		this.updating = false;
+
+	}
+
+	/**
+	 * Updates the input and output resources of a given graph.
+	 *
+	 * @param graph - The graph to update.
+	 */
+
+	private updateFrameGraph(graph: FrameGraph): void {
 
 	}
 
@@ -99,7 +192,7 @@ export class ResourceManager {
 		// Dispose orphaned resources.
 		for(const resource of this.activeResources) {
 
-			if(resource instanceof DisposableResource && !resources.has(resource)) {
+			if(isDisposable(resource) && !resources.has(resource)) {
 
 				resource.dispose();
 
