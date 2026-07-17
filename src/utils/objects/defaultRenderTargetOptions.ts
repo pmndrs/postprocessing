4492ceb121b812dcdc4dd1a1678e483166cf2e12
Raoul v. R. <vanruesc@outlook.de>
2026-07-17 12:56:07 +0200

    Create defaultRenderTargetOptions

---
+++ src/utils/objects/defaultRenderTargetOptions.ts
@@ -0,0 +1,11 @@
+import { RenderTargetOptions } from "three";
+
+/**
+ * Reusable default render target options.
+ *
+ * @internal
+ */
+
+export const defaultRenderTargetOptions: RenderTargetOptions = /* @__PURE__ */ {
+	depthBuffer: false
+};
--- src/utils/objects/index.ts
+++ src/utils/objects/index.ts
@@ -1,2 +1,3 @@
+export * from "./defaultRenderTargetOptions.js";
 export * from "./fullscreenCamera.js";
 export * from "./fullscreenGeometry.js";
