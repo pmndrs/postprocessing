0a0ea295376e3a7595226a88b11230008bd81ce1
Raoul v. R. <vanruesc@outlook.de>
2026-08-20 19:22:43 +0200

    Build manual locally and then move it to root

---
+++ apps/manual/move.js
@@ -0,0 +1,15 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+
+const src = path.join(import.meta.dirname, "public", "manual");
+const dest = path.join(import.meta.dirname, "..", "..", "public", "manual");
+
+try {
+
+	await fs.rename(src, dest);
+
+} catch(error) {
+
+	console.log(error);
+
+}
