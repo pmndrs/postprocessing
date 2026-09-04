40eaf68682c1a90783f48d8c2ab94e858090c21d
Raoul v. R. <vanruesc@outlook.de>
2026-09-04 18:29:24 +0200

    Turn RenderOperation into a base class

--- src/core/RenderOperation.ts
+++ src/core/RenderOperation.ts
@@ -1,5 +1,6 @@
 import { Compilable } from "./Compilable.js";
 import { Disposable } from "./Disposable.js";
+import { RenderTaskContext } from "./RenderTaskContext.js";
 import { Task } from "./Task.js";
 
 /**
@@ -8,4 +9,77 @@ import { Task } from "./Task.js";
  * @category Core
  */
 
-export interface RenderOperation extends Compilable, Disposable, Task {}
+export abstract class RenderOperation implements Compilable, Disposable, Task {
+
+	// #region Backing Data
+
+	/**
+	 * @see {@link name}
+	 */
+
+	private _name: string;
+
+	/**
+	 * @see {@link enabled}
+	 */
+
+	private _enabled: boolean;
+
+	// #endregion
+
+	/**
+	 * The context of the parent render task.
+	 */
+
+	protected readonly context: Readonly<RenderTaskContext>;
+
+	/**
+	 * Constructs a new render operation.
+	 *
+	 * @param name - The name of the operation.
+	 * @param context - The context of the parent render task.
+	 */
+
+	constructor(name: string, context: Readonly<RenderTaskContext>) {
+
+		this._name = name;
+		this._enabled = true;
+		this.context = context;
+
+	}
+
+	// #region Accessors
+
+	get name(): string {
+
+		return this._name;
+
+	}
+
+	protected set name(value: string) {
+
+		this._name = value;
+
+	}
+
+	get enabled(): boolean {
+
+		return this._enabled;
+
+	}
+
+	set enabled(value: boolean) {
+
+		this._enabled = value;
+
+	}
+
+	// #endregion
+
+	async compile(): Promise<void> {}
+
+	abstract execute(): void;
+
+	dispose(): void {}
+
+}
